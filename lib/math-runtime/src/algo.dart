f2f0821d172333210bdab8ee98f6e60bec86ebdc
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-16 14:13:55 +0100

    Create algo.dart

---
+++ lib/math-runtime/src/algo.dart
@@ -0,0 +1,16 @@
+/**
+ * mathe:buddy - a gamified app for higher math
+ * (c) 2022-2023 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+int gcd(int x, int y) {
+  while (y != 0) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
