5bc092c90f118738f1ae48b76e15af9699a6a07a
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-16 16:39:33 +0100

    Create dataExample.dart

---
+++ lib/compiler/src/data/dataExample.dart
@@ -0,0 +1,35 @@
+/**
+ * mathe:buddy - a gamified learning-app for higher math
+ * (c) 2022-2023 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import 'dataBlock.dart';
+
+// refer to the specification at https://app.f07-its.fh-koeln.de/docs-mbcl.html
+
+// -------- EXAMPLE --------
+
+class MBL_Example extends MBL_BlockItem {
+  String type = 'example';
+  items: (MBL_Equation | MBL_Text)[] = [];
+  
+  void postProcess() {
+    for(var i=0; i<this.items.length; i++) {
+      var item = this.items[i];
+      item.postProcess();
+    }
+  }
+  
+  Map<String,Object> toJSON() {
+    return {
+      "type": this.type,
+      "title": this.title,
+      "label": this.label,
+      "error": this.error,
+      "items": this.items.map((item) => item.toJSON()),
+    };
+  }
+}
