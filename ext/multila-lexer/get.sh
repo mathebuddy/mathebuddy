f2db85c93fd422977d58beee3545b9e67ff2b35d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-16 14:12:26 +0100

    Create get.sh

---
+++ ext/multila-lexer/get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p src/
+cd src/
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/src/lang.dart
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/src/lex.dart
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/src/state.dart
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/src/token.dart
+cd ..
+cd test/
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/test/example.dart
+curl -O https://raw.githubusercontent.com/multila/multila-lexer/main/test/lex_TESTS.dart
