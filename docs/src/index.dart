fe42533d791b7d272785fdd84b23e9077c96442b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-17 11:58:51 +0100

    Create index.dart

---
+++ docs/src/index.dart
@@ -0,0 +1,15 @@
+/**
+ * mathe:buddy - a gamified app for higher math
+ * (c) 2022-2023 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import 'math-runtime-playground.dart';
+import 'smpl-playground.dart';
+
+void main() {
+  mathRuntimePlayground();
+  smplPlayground();
+}
