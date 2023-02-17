c857af9a94651149bd12c9860ce163d3b9bc528d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-17 09:40:00 +0100

    Create install.py

---
+++ bin/install.py
@@ -0,0 +1,9 @@
+# mathe:buddy - a gamified learning-app for higher math
+# (c) 2022-2023 by TH Koeln
+# Author: Andreas Schwenk contact@compiler-construction.com
+# Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+# License: GPL-3.0-or-later
+
+import check
+
+print("TODO!")
