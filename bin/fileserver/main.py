800faaf58df8a4796c93ff62e0a31b512aad5d11
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-17 16:51:42 +0100

    Create main.py

---
+++ bin/fileserver/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=8000)
