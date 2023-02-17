069b83b57e8741dda171cf0187c832fd8f8393b1
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-17 20:03:01 +0100

    removed web/ directory from repo

--- web/README.md
+++
@@ -1,3 +0,0 @@
-# mathe:buddy Website
-
-Website for the MatheBuddy App, hosted at [https://app.f07-its.fh-koeln.de](https://app.f07-its.fh-koeln.de)
--- web/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-npm install
-
-wget -O docs-mbl.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-compiler/main/mbl.html
-wget -O docs-smpl.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-smpl/main/smpl.html
-wget -O docs-mbcl.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-compiler/main/mbcl.html
-wget -O docs-sim.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-simulator/main/sim.html
