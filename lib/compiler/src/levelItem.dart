95c308a3353a82053d77c6b1d40ab3eb0d45bc07
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-19 10:55:42 +0100

    Create levelItem.dart

---
+++ lib/compiler/src/levelItem.dart
@@ -0,0 +1,18 @@
+/**
+ * mathe:buddy - a gamified learning-app for higher math
+ * (c) 2022-2023 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import '../../mbcl/src/levelItem.dart';
+
+class MBCL_LevelItem extends MBCL_LevelItem__ABSTRACT {
+  MBCL_LevelItem(MBCL_LevelItemType type) : super(type);
+
+  @override
+  void postProcess() {
+    // TODO: implement postProcess
+  }
+}
