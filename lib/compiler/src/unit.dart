668bc962cd28d6ce1f77983ff77c959563aac33c
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-19 10:55:39 +0100

    Create unit.dart

---
+++ lib/compiler/src/unit.dart
@@ -0,0 +1,11 @@
+/**
+ * mathe:buddy - a gamified learning-app for higher math
+ * (c) 2022-2023 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import '../../mbcl/src/unit.dart';
+
+class MBCL_Unit extends MBCL_Unit__ABSTRACT {}
