ebf4e94f37ab71fc596c57bb9f80a61c5fa1f499
Andreas Schwenk <andi.schwenk@googlemail.com>
2023-02-20 15:00:55 +0100

    Update build-web.sh

--- app/mathebuddy/build-web.sh
+++ app/mathebuddy/build-web.sh
@@ -7,6 +7,12 @@ flutter build web --web-renderer auto --release  # auto := html for mobile, canv
 rm -rf ../../docs/sim
 cp -r build/web/ ../../docs/sim
 
-# replace <base href="/"> by <base href="/sim/">
+cp ../../docs/sim/index.html ../../docs/sim/index-github.html 
+
+# for localhost: replace <base href="/"> by <base href="/sim/">
 sed -i.bak 's/<base href="\/">/<base href="\/sim\/">/g' ../../docs/sim/index.html 
 rm ../../docs/sim/index.html.bak
+
+# for github pages: replace <base href="/"> by <base href="/mathebuddy-sim/">
+sed -i.bak 's/<base href="\/">/<base href="\/mathebuddy\/sim\/">/g' ../../docs/sim/index-github.html 
+rm ../../docs/sim/index-github.html.bak
