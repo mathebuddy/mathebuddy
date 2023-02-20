e941c868c00760cf1c0eff5c14cfa86d54eb5e47
Andreas Schwenk <andi.schwenk@googlemail.com>
2023-02-20 14:44:29 +0100

    Update build-web.sh

--- app/mathebuddy/build-web.sh
+++ app/mathebuddy/build-web.sh
@@ -2,3 +2,11 @@
 #flutter build web --web-renderer canvaskit --release
 #flutter build web --web-renderer html --release
 flutter build web --web-renderer auto --release  # auto := html for mobile, canvaskit for desktop
+
+# update website
+rm -rf ../../docs/sim
+cp -r build/web/ ../../docs/sim
+
+# replace <base href="/"> by <base href="/sim/">
+sed -i.bak 's/<base href="\/">/<base href="\/sim\/">/g' ../../docs/sim/index.html 
+rm ../../docs/sim/index.html.bak
