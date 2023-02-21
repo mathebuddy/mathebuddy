9a0896fe5479f59e25ba05d1f7666491911c24ff
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-21 18:12:00 +0100

    Update test.dart

--- lib/compiler/test/test.dart
+++ lib/compiler/test/test.dart
@@ -6,60 +6,72 @@
  * License: GPL-3.0-or-later
  */
 
-import * as fs from 'fs';
-import * as lz_string from 'lz-string';
+import 'dart:convert';
+import 'dart:io';
 
-import { Compiler } from '../src';
-
-console.log('mathe:buddy Compiler (c) 2022 by TH Koeln');
+import '../src/compiler.dart';
 
 // load function
-function load(path: string): string {
-  if (fs.existsSync(path) == false) return '';
-  return fs.readFileSync(path, 'utf-8');
+String load(String path) {
+  if (File(path).existsSync() == false)
+    return '';
+  else
+    return File(path).readAsStringSync();
 }
 
-// demo course
-console.log('=== TESTING DEMO COURSE ===');
-const compiler = new Compiler();
-compiler.compile('examples/demo-course/course.mbl', load);
-fs.writeFileSync(
-  'examples/demo-course/course_COMPILED.json',
-  JSON.stringify(compiler.getCourse().toJSON(), null, 2),
-);
-fs.writeFileSync(
-  'examples/demo-course/course_COMPILED.hex',
-  lz_string.compressToBase64(
-    JSON.stringify(compiler.getCourse().toJSON(), null, 0),
-  ),
-  'base64',
-);
+void main() {
+  print('mathe:buddy Compiler (c) 2022-2023 by TH Koeln');
 
-// demo files
-const inputPath = 'examples/';
-const files = fs.readdirSync(inputPath).sort();
-for (const file of files) {
-  /*if (file.includes('hello.mbl')) {
-    const bp = 1337;
-  }*/
-  const path = inputPath + file;
-  if (path.endsWith('.mbl') == false) continue;
-  console.log('=== TESTING FILE ' + path + ' ===');
-  // compile file
-  const compiler = new Compiler();
-  compiler.compile(path, load);
-  // write output as JSON
-  const outputPath =
-    inputPath + file.substring(0, file.length - 4) + '_COMPILED.json';
-  fs.writeFileSync(
-    outputPath,
+  // demo course
+  print('=== TESTING DEMO COURSE ===');
+  var compiler = new Compiler(load);
+  //compiler.compile('examples/demo-course/course.mbl');
+  compiler.compile('lib/compiler/test/data/demo-basic/hello.mbl');
+  var y = compiler.getCourse()?.toJSON();
+  var json = JsonEncoder().convert(y);
+  print(json);
+
+  var bp = 1337;
+
+  /*fs.writeFileSync(
+    'examples/demo-course/course_COMPILED.json',
     JSON.stringify(compiler.getCourse().toJSON(), null, 2),
   );
-  // write output as compressed HEX file
-  const outputCompressed = lz_string.compressToBase64(
-    JSON.stringify(compiler.getCourse().toJSON(), null, 0),
-  );
-  const outputPathCompressed =
-    inputPath + file.substring(0, file.length - 4) + '_COMPILED.hex';
-  fs.writeFileSync(outputPathCompressed, outputCompressed, 'base64');
+  fs.writeFileSync(
+    'examples/demo-course/course_COMPILED.hex',
+    lz_string.compressToBase64(
+      JSON.stringify(compiler.getCourse().toJSON(), null, 0),
+    ),
+    'base64',
+  );*/
+
+  /* TODO
+  // demo files
+  const inputPath = 'examples/';
+  const files = fs.readdirSync(inputPath).sort();
+  for (const file of files) {
+    /*if (file.includes('hello.mbl')) {
+      const bp = 1337;
+    }*/
+    const path = inputPath + file;
+    if (path.endsWith('.mbl') == false) continue;
+    print('=== TESTING FILE ' + path + ' ===');
+    // compile file
+    const compiler = new Compiler();
+    compiler.compile(path, load);
+    // write output as JSON
+    const outputPath =
+      inputPath + file.substring(0, file.length - 4) + '_COMPILED.json';
+    fs.writeFileSync(
+      outputPath,
+      JSON.stringify(compiler.getCourse().toJSON(), null, 2),
+    );
+    // write output as compressed HEX file
+    const outputCompressed = lz_string.compressToBase64(
+      JSON.stringify(compiler.getCourse().toJSON(), null, 0),
+    );
+    const outputPathCompressed =
+      inputPath + file.substring(0, file.length - 4) + '_COMPILED.hex';
+    fs.writeFileSync(outputPathCompressed, outputCompressed, 'base64');
+  }*/
 }
