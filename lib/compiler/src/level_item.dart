367f9317887768533769eb114ba8878f56ea0de2
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-22 19:49:22 +0100

    Update level_item.dart

--- lib/compiler/src/level_item.dart
+++ lib/compiler/src/level_item.dart
@@ -20,19 +20,23 @@ class MBCL_LevelItem extends MBCL_LevelItem__ABSTRACT {
       item.postProcess();
     }
     switch (this.type) {
-      case MBCL_LevelItemType.AlignLeft:
       case MBCL_LevelItemType.AlignCenter:
+      case MBCL_LevelItemType.AlignLeft:
       case MBCL_LevelItemType.AlignRight:
-      case MBCL_LevelItemType.Paragraph:
-      case MBCL_LevelItemType.InlineMath:
       case MBCL_LevelItemType.BoldText:
-      case MBCL_LevelItemType.ItalicText:
-      case MBCL_LevelItemType.Itemize:
+      case MBCL_LevelItemType.Color:
       case MBCL_LevelItemType.Enumerate:
       case MBCL_LevelItemType.EnumerateAlpha:
+      case MBCL_LevelItemType.InlineMath:
+      case MBCL_LevelItemType.ItalicText:
+      case MBCL_LevelItemType.Itemize:
+      case MBCL_LevelItemType.Paragraph:
       case MBCL_LevelItemType.Span:
-      case MBCL_LevelItemType.Color:
-        simplifyText(this.items);
+        aggregateText(this.items);
+        if (this.type == MBCL_LevelItemType.Paragraph) {
+          aggregateMultipleChoice(this.items);
+          aggregateSingleChoice(this.items);
+        }
         break;
       default:
         break;
@@ -40,7 +44,7 @@ class MBCL_LevelItem extends MBCL_LevelItem__ABSTRACT {
   }
 }
 
-void simplifyText(List<MBCL_LevelItem__ABSTRACT> items) {
+void aggregateText(List<MBCL_LevelItem__ABSTRACT> items) {
   // remove unnecessary line feeds
   while (items.length > 0 && items[0].type == MBCL_LevelItemType.LineFeed) {
     items.removeAt(0);
@@ -61,6 +65,34 @@ void simplifyText(List<MBCL_LevelItem__ABSTRACT> items) {
       items[i - 1].text += text;
       // TODO: next line is an ugly hack for TeX..
       items[i - 1].text = items[i - 1].text.replaceAll("\\ ", '\\');
+      items.removeAt(i);
+      i--;
+    }
+  }
+}
+
+void aggregateMultipleChoice(List<MBCL_LevelItem__ABSTRACT> items) {
+  for (var i = 0; i < items.length; i++) {
+    if (i > 0 &&
+        items[i - 1].type == MBCL_LevelItemType.MultipleChoice &&
+        items[i].type == MBCL_LevelItemType.MultipleChoice) {
+      var u = items[i - 1];
+      var v = items[i];
+      u.items.addAll(v.items);
+      items.removeAt(i);
+      i--;
+    }
+  }
+}
+
+void aggregateSingleChoice(List<MBCL_LevelItem__ABSTRACT> items) {
+  for (var i = 0; i < items.length; i++) {
+    if (i > 0 &&
+        items[i - 1].type == MBCL_LevelItemType.SingleChoice &&
+        items[i].type == MBCL_LevelItemType.SingleChoice) {
+      var u = items[i - 1];
+      var v = items[i];
+      u.items.addAll(v.items);
       items.removeAt(i);
       i--;
     }
