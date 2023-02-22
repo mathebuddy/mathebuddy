1c2aba87deb36ae1ffc88972d62abbe67b853d01
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-22 14:43:01 +0100

    Update build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-dart compile js src/index.dart -O4 -o index.min.js
+dart compile js src/index.dart -O4 -o js/index.min.js
 
-cp ../lib/math-runtime/grammar.txt math-runtime-grammar.txt
-cp ../lib/smpl/grammar.txt smpl-grammar.txt
-cp ../lib/compiler/grammar.txt mbl-grammar.txt
+cp ../lib/math-runtime/grammar.txt text/math-runtime-grammar.txt
+cp ../lib/smpl/grammar.txt text/smpl-grammar.txt
+cp ../lib/compiler/grammar.txt text/mbl-grammar.txt
 
-python3 update.py
+python3 build.py
