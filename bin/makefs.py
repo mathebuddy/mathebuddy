661bf31c94444a3649afd1018cadff6461e0e00c
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-22 12:13:13 +0100

    Update makefs.py

--- bin/makefs.py
+++ bin/makefs.py
@@ -4,13 +4,53 @@
 # Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 # License: GPL-3.0-or-later
 
+import sys
+import os
+import json
+import glob
+
+# This file recursively creates textfiles that describe the contests of a directory,
+# staring from a argument given base path.
+# For each sub-directory, a file named "_fs.txt" is created. It contains each file of that
+# directory in a separate line. Directories are written with forward-slash at the end.
+
+# Why? Many Webservers (e.g. github pages) do not allow directory listening for security reasons.
+
+# Usage: "python3 makefs.py BASE_PATH"
+
+if len(sys.argv) < 2:
+    print("ERROR: makefs.py expects a PATH argument (e.g. 'python3 makefs.py my-path')")
+    exit(-1)
+
+input_path = sys.argv[1]
+if not input_path.endswith('/'):
+    input_path = input_path + '/'
+
+
+def build_dir(path):
+    fs = ''
+    files = glob.glob(path + '*')
+    for file in files:
+        base = os.path.basename(file)
+        if os.path.isdir(file):
+            build_dir(file + '/')
+            fs += base + '/\n'
+        else:
+            fs += base + '\n'
+    f = open(path + '_fs.txt', "w")
+    f.write(fs)
+    f.close()
+
+
+build_dir(input_path)
+
+exit(0)
+
+# ---------------- old src. TODO: remove? ----------
+
 # This file builds a JSON output that represents a filesystem
 # Usage: e.g. to update /docs/demo/fs.json in this repository
 
-import glob
-import json
-import os
-import sys
 
 if len(sys.argv) < 2:
     print("ERROR: makefs.py expects a PATH argument (e.g. 'python3 makefs.py my-path/')")
