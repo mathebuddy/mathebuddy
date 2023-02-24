14caf8397b5a11e0c8c711f8880580801975ab51
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-24 14:48:11 +0100

    Update unit.dart

--- lib/mbcl/src/unit.dart
+++ lib/mbcl/src/unit.dart
@@ -1,34 +1,32 @@
-/**
- * mathe:buddy - a gamified learning-app for higher math
- * (c) 2022-2023 by TH Koeln
- * Author: Andreas Schwenk contact@compiler-construction.com
- * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
- * License: GPL-3.0-or-later
- */
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
 
 // refer to the specification at https://mathebuddy.github.io/mathebuddy/ (TODO: update link!)
 
 import 'level.dart';
 
-class MBCL_Unit {
+class MbclUnit {
   String title = '';
-  List<MBCL_Level> levels = [];
+  List<MbclLevel> levels = [];
 
   Map<String, dynamic> toJSON() {
     return {
-      "title": this.title,
-      "levels": this.levels.map((level) => level.fileId).toList(),
+      "title": title,
+      "levels": levels.map((level) => level.fileId).toList(),
     };
   }
 
   fromJSON(Map<String, dynamic> src) {
-    this.title = src["title"];
-    this.levels = [];
+    title = src["title"];
+    levels = [];
     int n = src["levels"].length;
     for (var i = 0; i < n; i++) {
-      var level = new MBCL_Level();
+      var level = MbclLevel();
       level.fromJSON(src["levels"][i]);
-      this.levels.add(level);
+      levels.add(level);
     }
   }
 }
