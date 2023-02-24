599636b1e8eab2fc67770d27e2aa3e1251127cf0
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-24 11:14:05 +0100

    Update level.dart

--- lib/mbcl/src/level.dart
+++ lib/mbcl/src/level.dart
@@ -10,32 +10,49 @@
 
 import 'level_item.dart';
 
-abstract class MBCL_Level__ABSTRACT {
+class MBCL_Level {
   String fileId =
       ''; // all references go here; label is only used for searching
   String title = '';
   String label = '';
-  int pos_x = -1;
-  int pos_y = -1;
-  List<MBCL_Level__ABSTRACT> requires = [];
+  int posX = -1;
+  int posY = -1;
+  List<MBCL_Level> requires = [];
   List<String> requires_tmp = []; // only used while compiling
-  List<MBCL_LevelItem__ABSTRACT> items = [];
-
-  void postProcess();
+  List<MBCL_LevelItem> items = [];
 
   Map<String, dynamic> toJSON() {
     return {
       "fileId": this.fileId,
       "title": this.title,
       "label": this.label,
-      "posX": this.pos_x,
-      "posY": this.pos_y,
+      "posX": this.posX,
+      "posY": this.posY,
       "requires": this.requires.map((req) => req.fileId).toList(),
       "items": this.items.map((item) => item.toJSON()).toList(),
     };
   }
 
   fromJSON(Map<String, dynamic> src) {
-    // TODO
+    this.fileId = src["fileId"];
+    this.title = src["title"];
+    this.label = src["label"];
+    this.posX = src["posX"];
+    this.posY = src["posY"];
+    // requires
+    this.requires = [];
+    int n = src["requires"].length;
+    for (var i = 0; i < n; i++) {
+      this.requires_tmp.add(src["requires"][i]);
+      // TODO: this.requires!!!
+    }
+    // items
+    this.items = [];
+    n = src["items"].length;
+    for (var i = 0; i < n; i++) {
+      var item = new MBCL_LevelItem(MBCL_LevelItemType.Error);
+      item.fromJSON(src["items"][i]);
+      this.items.add(item);
+    }
   }
 }
