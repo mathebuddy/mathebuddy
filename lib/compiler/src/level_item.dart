5868d38154b33e3ad35bcdf182d8de71833cce26
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-24 14:47:53 +0100

    Update level_item.dart

--- lib/compiler/src/level_item.dart
+++ lib/compiler/src/level_item.dart
@@ -8,26 +8,26 @@
 
 import '../../mbcl/src/level_item.dart';
 
-void postProcessLevelItem(MBCL_LevelItem levelItem) {
+void postProcessLevelItem(MbclLevelItem levelItem) {
   for (var i = 0; i < levelItem.items.length; i++) {
     var item = levelItem.items[i];
     postProcessLevelItem(item);
   }
   switch (levelItem.type) {
-    case MBCL_LevelItemType.AlignCenter:
-    case MBCL_LevelItemType.AlignLeft:
-    case MBCL_LevelItemType.AlignRight:
-    case MBCL_LevelItemType.BoldText:
-    case MBCL_LevelItemType.Color:
-    case MBCL_LevelItemType.Enumerate:
-    case MBCL_LevelItemType.EnumerateAlpha:
-    case MBCL_LevelItemType.InlineMath:
-    case MBCL_LevelItemType.ItalicText:
-    case MBCL_LevelItemType.Itemize:
-    case MBCL_LevelItemType.Paragraph:
-    case MBCL_LevelItemType.Span:
+    case MbclLevelItemType.alignCenter:
+    case MbclLevelItemType.alignLeft:
+    case MbclLevelItemType.alignRight:
+    case MbclLevelItemType.boldText:
+    case MbclLevelItemType.color:
+    case MbclLevelItemType.enumerate:
+    case MbclLevelItemType.enumerateAlpha:
+    case MbclLevelItemType.inlineMath:
+    case MbclLevelItemType.italicText:
+    case MbclLevelItemType.itemize:
+    case MbclLevelItemType.paragraph:
+    case MbclLevelItemType.span:
       aggregateText(levelItem.items);
-      if (levelItem.type == MBCL_LevelItemType.Paragraph) {
+      if (levelItem.type == MbclLevelItemType.paragraph) {
         aggregateMultipleChoice(levelItem.items);
         aggregateSingleChoice(levelItem.items);
       }
@@ -37,20 +37,20 @@ void postProcessLevelItem(MBCL_LevelItem levelItem) {
   }
 }
 
-void aggregateText(List<MBCL_LevelItem> items) {
+void aggregateText(List<MbclLevelItem> items) {
   // remove unnecessary line feeds
-  while (items.length > 0 && items[0].type == MBCL_LevelItemType.LineFeed) {
+  while (items.length > 0 && items[0].type == MbclLevelItemType.lineFeed) {
     items.removeAt(0);
   }
   while (items.length > 0 &&
-      items[items.length - 1].type == MBCL_LevelItemType.LineFeed) {
+      items[items.length - 1].type == MbclLevelItemType.lineFeed) {
     items.removeLast();
   }
   // concatenate consecutive text items
   for (var i = 0; i < items.length; i++) {
     if (i > 0 &&
-        items[i - 1].type == MBCL_LevelItemType.Text &&
-        items[i].type == MBCL_LevelItemType.Text) {
+        items[i - 1].type == MbclLevelItemType.text &&
+        items[i].type == MbclLevelItemType.text) {
       var text = items[i].text;
       if ('.,:!?'.contains(text) == false) {
         text = ' ' + text;
@@ -64,11 +64,11 @@ void aggregateText(List<MBCL_LevelItem> items) {
   }
 }
 
-void aggregateMultipleChoice(List<MBCL_LevelItem> items) {
+void aggregateMultipleChoice(List<MbclLevelItem> items) {
   for (var i = 0; i < items.length; i++) {
     if (i > 0 &&
-        items[i - 1].type == MBCL_LevelItemType.MultipleChoice &&
-        items[i].type == MBCL_LevelItemType.MultipleChoice) {
+        items[i - 1].type == MbclLevelItemType.multipleChoice &&
+        items[i].type == MbclLevelItemType.multipleChoice) {
       var u = items[i - 1];
       var v = items[i];
       u.items.addAll(v.items);
@@ -78,11 +78,11 @@ void aggregateMultipleChoice(List<MBCL_LevelItem> items) {
   }
 }
 
-void aggregateSingleChoice(List<MBCL_LevelItem> items) {
+void aggregateSingleChoice(List<MbclLevelItem> items) {
   for (var i = 0; i < items.length; i++) {
     if (i > 0 &&
-        items[i - 1].type == MBCL_LevelItemType.SingleChoice &&
-        items[i].type == MBCL_LevelItemType.SingleChoice) {
+        items[i - 1].type == MbclLevelItemType.singleChoice &&
+        items[i].type == MbclLevelItemType.singleChoice) {
       var u = items[i - 1];
       var v = items[i];
       u.items.addAll(v.items);
