6c1da68e055f78c90a153adb5c90564424e1e1cf
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-02-24 14:48:02 +0100

    Update course.dart

--- lib/mbcl/src/course.dart
+++ lib/mbcl/src/course.dart
@@ -1,40 +1,38 @@
-/**
- * mathe:buddy - a gamified learning-app for higher math
- * (c) 2022-2023 by TH Koeln
- * Author: Andreas Schwenk contact@compiler-construction.com
- * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
- * License: GPL-3.0-or-later
- */
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
 
 // refer to the specification at https://mathebuddy.github.io/mathebuddy/ (TODO: update link!)
 
 import 'chapter.dart';
 
-enum MBCL_Course_Debug {
-  No,
-  Chapter,
-  Level,
+enum MbclCourseDebug {
+  no,
+  chapter,
+  level,
 }
 
-class MBCL_Course {
-  MBCL_Course_Debug debug = MBCL_Course_Debug.No;
+class MbclCourse {
+  MbclCourseDebug debug = MbclCourseDebug.no;
   String title = '';
   String author = '';
   int mbclVersion = 1;
   int dateModified = (DateTime.now().millisecondsSinceEpoch / 1000).floor();
-  List<MBCL_Chapter> chapters = [];
+  List<MbclChapter> chapters = [];
 
-  MBCL_Chapter? getChapterByLabel(String label) {
-    for (var i = 0; i < this.chapters.length; i++) {
-      var chapter = this.chapters[i];
+  MbclChapter? getChapterByLabel(String label) {
+    for (var i = 0; i < chapters.length; i++) {
+      var chapter = chapters[i];
       if (chapter.label == label) return chapter;
     }
     return null;
   }
 
-  MBCL_Chapter? getChapterByFileID(String fileID) {
-    for (var i = 0; i < this.chapters.length; i++) {
-      var chapter = this.chapters[i];
+  MbclChapter? getChapterByFileID(String fileID) {
+    for (var i = 0; i < chapters.length; i++) {
+      var chapter = chapters[i];
       if (chapter.fileId == fileID) return chapter;
     }
     return null;
@@ -42,27 +40,27 @@ class MBCL_Course {
 
   Map<String, Object> toJSON() {
     return {
-      "debug": this.debug.name,
-      "title": this.title,
-      "author": this.author,
-      "mbclVersion": this.mbclVersion,
-      "dateModified": this.dateModified,
-      "chapters": this.chapters.map((chapter) => chapter.toJSON()).toList(),
+      "debug": debug.name,
+      "title": title,
+      "author": author,
+      "mbclVersion": mbclVersion,
+      "dateModified": dateModified,
+      "chapters": chapters.map((chapter) => chapter.toJSON()).toList(),
     };
   }
 
   fromJSON(Map<String, dynamic> src) {
-    this.debug = MBCL_Course_Debug.values.byName(src["debug"]);
-    this.title = src["title"];
-    this.author = src["author"];
-    this.mbclVersion = src["mbclVersion"];
-    this.dateModified = src["dateModified"];
-    this.chapters = [];
+    debug = MbclCourseDebug.values.byName(src["debug"]);
+    title = src["title"];
+    author = src["author"];
+    mbclVersion = src["mbclVersion"];
+    dateModified = src["dateModified"];
+    chapters = [];
     int n = src["chapters"].length;
     for (var i = 0; i < n; i++) {
-      var chapter = new MBCL_Chapter();
+      var chapter = MbclChapter();
       chapter.fromJSON(src["chapters"][i]);
-      this.chapters.add(chapter);
+      chapters.add(chapter);
     }
   }
 }
