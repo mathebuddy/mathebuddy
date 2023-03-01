885b490bcf76f3bbf386b922aabe456d1651ad0f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-01 19:39:16 +0100

    Update test.dart

--- lib/tex/test/test.dart
+++ lib/tex/test/test.dart
@@ -11,9 +11,16 @@ import 'dart:io';
 import '../src/tex.dart';
 
 void main() {
-  var tex = new Tex();
-  var src = "3x+y^2"; //"\\frac x{ \\sum_1^{{6}} w } \\cdot 5";
-  var output = tex.tex2svg(src);
+  var tex = new TeX();
+  var src =
+      "f(x,y)=3x+y^{2^{8+1}}+z^{3+2}+\\alpha_{\\gamma}+\\beta+X"; //"\\frac x{ \\sum_1^{{6}} w } \\cdot 5";
+  var output = '';
+  var paintBox = true;
+  output = tex.tex2svg(src, paintBox);
+  if (output.isEmpty) {
+    print("ERROR: tex2svg failed: " + tex.error);
+    assert(false);
+  }
   print(output);
   File('lib/tex/test/svg/test.svg').writeAsStringSync(output);
 }
