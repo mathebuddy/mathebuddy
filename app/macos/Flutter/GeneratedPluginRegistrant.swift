5c2087a6c1b310e9473d31c686ddea90577a11f5
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-04 13:39:36 +0100

    removed flutter_js dependency

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_js
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
 }
