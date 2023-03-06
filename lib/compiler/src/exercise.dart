2acfaca53af31eedef94f60ca2d8e44967d8437d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-06 13:38:31 +0100

    Update exercise.dart

--- lib/compiler/src/exercise.dart
+++ lib/compiler/src/exercise.dart
@@ -1,10 +1,8 @@
-/**
- * mathe:buddy - a gamified learning-app for higher math
- * (c) 2022-2023 by TH Koeln
- * Author: Andreas Schwenk contact@compiler-construction.com
- * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
- * License: GPL-3.0-or-later
- */
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
 
 import '../../math-runtime/src/operand.dart';
 
@@ -12,19 +10,20 @@ import '../../mbcl/src/level_item.dart';
 
 // TODO: move exercise related code from block.dart to here
 
-const NUM_INST = 3; // TODO!! must be configurable
+const numberOfInstances = 3; // TODO!! must be configurable
 
 String addStaticBooleanVariable(MbclExerciseData data, bool value) {
-  var varId = '__bool__' + (data.staticVariableCounter___++).toString();
+  var varId = '__bool__${data.staticVariableCounter}';
+  data.staticVariableCounter++;
   data.variables.add(varId);
-  data.smplOperandType___[varId] = OperandType.BOOLEAN.name;
-  if (data.instances.length == 0) {
-    for (var i = 0; i < NUM_INST; i++) {
+  data.smplOperandType[varId] = OperandType.boolean.name;
+  if (data.instances.isEmpty) {
+    for (var i = 0; i < numberOfInstances; i++) {
       Map<String, String> instance = {};
       data.instances.add(instance);
     }
   }
-  for (var i = 0; i < NUM_INST; i++) {
+  for (var i = 0; i < numberOfInstances; i++) {
     data.instances[i][varId] = value ? 'true' : 'false';
   }
   return varId;
