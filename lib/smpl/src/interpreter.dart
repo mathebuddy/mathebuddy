88f624becfcc6bf5d343b229d6bbbea3d99359cc
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-06 13:39:15 +0100

    Update interpreter.dart

--- lib/smpl/src/interpreter.dart
+++ lib/smpl/src/interpreter.dart
@@ -1,65 +1,62 @@
-/**
- * mathe:buddy - a gamified learning-app for higher math
- * (c) 2022-2023 by TH Koeln
- * Author: Andreas Schwenk contact@compiler-construction.com
- * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
- * License: GPL-3.0-or-later
- */
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
 
 import '../../math-runtime/src/operand.dart';
 import '../../math-runtime/src/term.dart';
-import '../../math-runtime/src/parse.dart' as termParser;
+import '../../math-runtime/src/parse.dart' as term_parser;
+
 import 'parser.dart';
 
 class InterpreterSymbol {
   String id = '';
-  Term term = Term.Const(Operand.createInt(0));
+  Term term = Term.createConst(Operand.createInt(0));
   Operand value = Operand.createInt(0);
 }
 
 class Interpreter {
   List<Map<String, InterpreterSymbol>> _symbolTable = [];
-  termParser.Parser _termParser = new termParser.Parser();
+  final term_parser.Parser _termParser = term_parser.Parser();
 
   Interpreter() {
     // TODO: term parser splits variable names of length > 1
   }
 
-  Map<String, InterpreterSymbol> runProgram(AST_Node program) {
+  Map<String, InterpreterSymbol> runProgram(AstNode program) {
     // init
-    this._symbolTable = [];
-    this._symbolTable.add({}); // open a new scope
+    _symbolTable = [];
+    _symbolTable.add({}); // open a new scope
     // run
-    this._run(program);
-    return this._symbolTable[0];
+    _run(program);
+    return _symbolTable[0];
   }
 
-  void _run(AST_Node node) {
+  void _run(AstNode node) {
     if (node is StatementList) {
       // ----- statement list -----
       var statementList = node;
-      if (statementList.createScope)
-        this._symbolTable.add({}); // open a new scope
+      if (statementList.createScope) _symbolTable.add({}); // open a new scope
       for (var i = 0; i < statementList.statements.length; i++) {
         var statement = statementList.statements[i];
-        this._run(statement);
+        _run(statement);
       }
-      if (statementList.createScope)
-        this._symbolTable.removeLast(); // close scope
+      if (statementList.createScope) _symbolTable.removeLast(); // close scope
     } else if (node is Assignment) {
       // ----- assignment -----
       var assignment = node;
-      InterpreterSymbol? symbol = null;
+      InterpreterSymbol? symbol;
       if (assignment.createSymbol) {
-        symbol = this._addSymbol(assignment.lhs);
+        symbol = _addSymbol(assignment.lhs);
       } else {
-        symbol = this._getSymbol(assignment.lhs);
+        symbol = _getSymbol(assignment.lhs);
       }
-      if (symbol == null)
-        this._error('symbol "${assignment.lhs}" is unknown.'); // TODO: location
-      else {
-        symbol.term = this._processTerm(assignment.rhs, assignment.vars);
-        if (assignment.vars.length == 0) {
+      if (symbol == null) {
+        _error('symbol "${assignment.lhs}" is unknown.'); // TODO: location
+      } else {
+        symbol.term = _processTerm(assignment.rhs, assignment.vars);
+        if (assignment.vars.isEmpty) {
           var isOK = true;
           var k = 0;
           do {
@@ -67,26 +64,22 @@ class Interpreter {
             isOK = true;
             for (var i = 0; i < assignment.independentTo.length; i++) {
               var indTo = assignment.independentTo[i];
-              var s2 = this._getSymbol(indTo);
+              var s2 = _getSymbol(indTo);
               if (Operand.compareEqual(symbol.value, s2?.value as Operand)) {
                 isOK = false;
                 break;
               }
             }
             k++;
-            if (k >= 50)
-              this._error(
+            if (k >= 50) {
+              _error(
                 'failed to create different variable values for "${symbol.id}"',
               );
+            }
           } while (isOK == false);
           print(
-            '>> assigned ' +
-                symbol.id +
-                ' := ' +
-                symbol.value.toString() +
-                ' (' +
-                symbol.value.type.name +
-                ')',
+            '>> assigned ${symbol.id}'
+            ' := ${symbol.value} (${symbol.value.type.name})',
           );
         }
       }
@@ -94,35 +87,38 @@ class Interpreter {
       // ----- while loop -----
       var whileLoop = node;
       for (;;) {
-        var condition = this._processTerm(
-            whileLoop.condition, []).eval({}); // TODO: error handling
-        if (condition.type != OperandType.BOOLEAN)
-          this._error(
-              'while loop expects a boolean condition'); // TODO: location
+        var condition = _processTerm(whileLoop.condition, [])
+            .eval({}); // TODO: error handling
+        if (condition.type != OperandType.boolean) {
+          _error('while loop expects a boolean condition'); // TODO: location
+        }
         if (condition.real == 0) break;
-        this._run(whileLoop.statements as StatementList);
+        _run(whileLoop.statements as StatementList);
       }
     } else if (node is IfCond) {
       // ----- if condition -----
       var ifCond = node;
-      var condition = this
-          ._processTerm(ifCond.condition, []).eval({}); // TODO: error handling
-      if (condition.type != OperandType.BOOLEAN)
-        this._error('while loop expects a boolean condition'); // TODO: location
-      if (condition.real != 0)
-        this._run(ifCond.statementsTrue as StatementList);
-      else if (ifCond.statementsFalse != null)
-        this._run(ifCond.statementsFalse as StatementList);
+      var condition =
+          _processTerm(ifCond.condition, []).eval({}); // TODO: error handling
+      if (condition.type != OperandType.boolean) {
+        _error('while loop expects a boolean condition'); // TODO: location
+      }
+      if (condition.real != 0) {
+        _run(ifCond.statementsTrue as StatementList);
+      } else if (ifCond.statementsFalse != null) {
+        _run(ifCond.statementsFalse as StatementList);
+      }
     } else {
-      throw new Exception('unimplemented');
+      throw Exception('unimplemented');
     }
   }
 
   InterpreterSymbol _addSymbol(String id) {
-    var scope = this._symbolTable[this._symbolTable.length - 1];
-    if (scope.containsKey(id))
-      this._error('symbol "${id}" already exists.'); // TODO: location
-    var symbol = new InterpreterSymbol();
+    var scope = _symbolTable[_symbolTable.length - 1];
+    if (scope.containsKey(id)) {
+      _error('symbol "$id" already exists.'); // TODO: location
+    }
+    var symbol = InterpreterSymbol();
     symbol.id = id;
     scope[id] = symbol;
     return symbol;
@@ -130,29 +126,29 @@ class Interpreter {
 
   InterpreterSymbol? _getSymbol(String id) {
     if (id.startsWith('@')) id = id.substring(1);
-    var n = this._symbolTable.length; // number of scopes
+    var n = _symbolTable.length; // number of scopes
     for (var i = n - 1; i >= 0; i--) {
-      var scope = this._symbolTable[i];
+      var scope = _symbolTable[i];
       if (scope.containsKey(id)) return scope[id];
     }
     return null;
   }
 
   Term _processTerm(String src, List<String> keepVariables) {
-    Term? term = null;
+    Term? term;
     try {
-      term = this._termParser.parse(src);
+      term = _termParser.parse(src);
     } catch (e) {
-      this._error('error in term "${src}":' + e.toString()); // TODO: location
+      _error('error in term "$src":$e'); // TODO: location
     }
     term = term as Term;
     var variables = term.getVariableIDs();
     for (var id in variables) {
       if (keepVariables.contains(id)) continue;
-      var symbol = this._getSymbol(id);
+      var symbol = _getSymbol(id);
       if (symbol == null) {
-        this._error(
-            'error in term "${src}": variable ${id} is unknown!'); // TODO: location
+        _error(
+            'error in term "$src": variable $id is unknown!'); // TODO: location
       } else {
         if (id.startsWith('@')) {
           term.substituteVariableByTerm(id, symbol.term.clone());
@@ -161,11 +157,11 @@ class Interpreter {
         }
       }
     }
-    print('>> ' + term.toString());
+    print('>> ${term.toString()}');
     return term;
   }
 
   void _error(String message) {
-    throw new Exception(message); // TODO: location
+    throw Exception(message); // TODO: location
   }
 }
