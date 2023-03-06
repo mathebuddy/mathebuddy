8fe2cb250243c980cf3c182aee09eadb0913c166
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-06 13:38:15 +0100

    Update index.dart

--- docs/src/index.dart
+++ docs/src/index.dart
@@ -1,22 +1,20 @@
-/**
- * mathe:buddy - a gamified app for higher math
- * (c) 2022-2023 by TH Koeln
- * Author: Andreas Schwenk contact@compiler-construction.com
- * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
- * License: GPL-3.0-or-later
- */
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
 
-import 'math-runtime-playground.dart' as mathPlay;
-import 'smpl-playground.dart' as smplPlay;
-import 'mbl-playground.dart' as mblPlay;
-import 'tex-playground.dart' as texPlay;
+import 'math_runtime_playground.dart' as math_play;
+import 'smpl_playground.dart' as smpl_play;
+import 'mbl_playground.dart' as mbl_play;
+import 'tex_playground.dart' as tex_play;
 import 'sim.dart' as sim;
 
 void main() async {
-  mathPlay.mathRuntimePlayground();
-  smplPlay.smplPlayground();
-  mblPlay.mblPlayground();
-  texPlay.texPlayground();
+  math_play.mathRuntimePlayground();
+  smpl_play.smplPlayground();
+  mbl_play.mblPlayground();
+  tex_play.texPlayground();
   sim.init();
   //var files = await getFilesFromDir('demo/');
   //print(files.toString());
