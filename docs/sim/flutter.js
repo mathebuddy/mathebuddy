cea2e1b987be1df8f24cd49dcf60fd1188a2d91f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-09 17:41:55 +0100

    rebuild

--- docs/sim/flutter.js
+++ docs/sim/flutter.js
@@ -9,21 +9,6 @@ _flutter.loader = null;
 
 (function () {
   "use strict";
-
-  const baseUri = ensureTrailingSlash(getBaseURI());
-
-  function getBaseURI() {
-    const base = document.querySelector("base");
-    return (base && base.getAttribute("href")) || "";
-  }
-
-  function ensureTrailingSlash(uri) {
-    if (uri == "") {
-      return uri;
-    }
-    return uri.endsWith("/") ? uri : `${uri}/`;
-  }
-
   /**
    * Wraps `promise` in a timeout of the given `duration` in ms.
    *
@@ -124,7 +109,8 @@ _flutter.loader = null;
       }
       const {
         serviceWorkerVersion,
-        serviceWorkerUrl = `${baseUri}flutter_service_worker.js?v=${serviceWorkerVersion}`,
+        serviceWorkerUrl = "flutter_service_worker.js?v=" +
+          serviceWorkerVersion,
         timeoutMillis = 4000,
       } = settings;
 
@@ -242,7 +228,7 @@ _flutter.loader = null;
      * Returns undefined when an `onEntrypointLoaded` callback is supplied in `options`.
      */
     async loadEntrypoint(options) {
-      const { entrypointUrl = `${baseUri}main.dart.js`, onEntrypointLoaded } =
+      const { entrypointUrl = "main.dart.js", onEntrypointLoaded } =
         options || {};
 
       return this._loadEntrypoint(entrypointUrl, onEntrypointLoaded);
