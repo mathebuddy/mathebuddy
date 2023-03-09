092f0ad2e7aec6e6ee940264c7fa49b4007d1240
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-09 17:42:21 +0100

    Create paragraph.dart

---
+++ app/lib/paragraph.dart
@@ -0,0 +1,211 @@
+/// mathe:buddy - a gamified app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+import 'package:mathebuddy/keyboard_layouts.dart';
+import 'package:tex/tex.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:mathebuddy/mbcl/src/level_item.dart';
+
+import 'package:mathebuddy/color.dart';
+import 'package:mathebuddy/main.dart';
+
+const double defaultFontSize = 16;
+
+InlineSpan generateParagraphItem(CoursePageState state, MbclLevelItem item,
+    {bold = false,
+    italic = false,
+    color = Colors.black,
+    MbclExerciseData? exerciseData}) {
+  switch (item.type) {
+    case MbclLevelItemType.reference:
+      {
+        return TextSpan(
+            text: "REFERENCES_NOT_YET_IMPLEMENTED",
+            style: TextStyle(color: Colors.red));
+      }
+    case MbclLevelItemType.text:
+      return TextSpan(
+        text: "${item.text} ",
+        style: TextStyle(
+            color: color,
+            fontSize: defaultFontSize,
+            fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+            fontStyle: italic ? FontStyle.italic : FontStyle.normal),
+      );
+    case MbclLevelItemType.boldText:
+    case MbclLevelItemType.italicText:
+    case MbclLevelItemType.color:
+      {
+        List<InlineSpan> gen = [];
+        switch (item.type) {
+          case MbclLevelItemType.boldText:
+            {
+              for (var it in item.items) {
+                gen.add(generateParagraphItem(state, it,
+                    bold: true, exerciseData: exerciseData));
+              }
+              return TextSpan(children: gen);
+            }
+          case MbclLevelItemType.italicText:
+            {
+              for (var it in item.items) {
+                gen.add(generateParagraphItem(state, it,
+                    italic: true, exerciseData: exerciseData));
+              }
+              return TextSpan(
+                children: gen,
+              );
+            }
+          case MbclLevelItemType.color:
+            {
+              var colorKey = int.parse(item.id);
+              var colors = [
+                // TODO
+                Colors.black,
+                Colors.red,
+                Colors.blue,
+                Colors.purple,
+                Colors.orange
+              ];
+              var color = colors[colorKey % colors.length];
+              for (var it in item.items) {
+                gen.add(generateParagraphItem(state, it,
+                    color: color, exerciseData: exerciseData));
+              }
+              return TextSpan(children: gen);
+            }
+          default:
+            // this will never happen
+            return TextSpan();
+        }
+      }
+    case MbclLevelItemType.inlineMath:
+      {
+        var texSrc = '';
+        for (var subItem in item.items) {
+          switch (subItem.type) {
+            case MbclLevelItemType.text:
+              {
+                texSrc += subItem.text;
+                break;
+              }
+            case MbclLevelItemType.variableReference:
+              {
+                var variableId = subItem.id;
+                if (exerciseData == null) {
+                  texSrc += 'ERROR: not in exercise mode!';
+                } else {
+                  var instance =
+                      exerciseData.instances[exerciseData.runInstanceIdx];
+                  var variableValue = instance[variableId];
+                  if (variableValue == null) {
+                    texSrc += 'ERROR: unknown exercise variable $variableId';
+                  } else {
+                    texSrc += variableValue;
+                  }
+                }
+                break;
+              }
+            default:
+              print(
+                  "ERROR: genParagraphItem(..): type '${item.type.name}' is not finally implemented");
+          }
+        }
+        var tex = TeX();
+        tex.scalingFactor = 1.17;
+        var svg = tex.tex2svg(texSrc);
+        var svgWidth = tex.width;
+        if (svg.isEmpty) {
+          return TextSpan(
+            text: "${tex.error} ",
+            style: TextStyle(color: Colors.red, fontSize: defaultFontSize),
+          );
+        } else {
+          return WidgetSpan(
+              alignment: PlaceholderAlignment.middle,
+              child: SvgPicture.string(
+                svg,
+                width: svgWidth.toDouble(),
+              ));
+        }
+      }
+    case MbclLevelItemType.inputField:
+      {
+        var inputFieldData = item.inputFieldData as MbclInputFieldData;
+        if (exerciseData != null &&
+            exerciseData.inputFields.containsKey(item.id) == false) {
+          exerciseData.inputFields[item.id] = inputFieldData;
+          inputFieldData.studentValue = "";
+          var exerciseInstance =
+              exerciseData.instances[exerciseData.runInstanceIdx];
+          inputFieldData.expectedValue =
+              exerciseInstance[inputFieldData.variableId] as String;
+        }
+        Widget contents;
+        if (inputFieldData.studentValue.isEmpty) {
+          contents = Icon(
+            Icons.keyboard,
+            size: 42,
+            color: matheBuddyRed,
+          );
+        } else {
+          var tex = TeX();
+          tex.scalingFactor = 1.17;
+          var svg = tex.tex2svg(inputFieldData.studentValue);
+          var svgWidth = tex.width;
+          if (svg.isEmpty) {
+            contents = Text(
+              "${tex.error} ",
+              style: TextStyle(color: Colors.red, fontSize: defaultFontSize),
+            );
+          } else {
+            contents = RichText(
+                text: WidgetSpan(
+                    alignment: PlaceholderAlignment.middle,
+                    child: SvgPicture.string(
+                      svg,
+                      width: svgWidth.toDouble(),
+                      color: matheBuddyRed,
+                    )));
+          }
+          /*contents = Text(inputFieldData.studentValue,
+              style:
+                  TextStyle(color: matheBuddyRed, fontSize: defaultFontSize));*/
+        }
+        return WidgetSpan(
+            alignment: PlaceholderAlignment.middle,
+            child: GestureDetector(
+                onTap: () {
+                  state.keyboardState.exerciseData = exerciseData;
+                  state.keyboardState.inputFieldData = inputFieldData;
+                  switch (inputFieldData.type) {
+                    case MbclInputFieldType.int:
+                      state.keyboardState.layout = integerKeyboardLayout;
+                      break;
+                    default:
+                      print("WARNING: generateParagraphItem():"
+                          "keyboard layout for input field type"
+                          " ${inputFieldData.type.name} not yet implemented");
+                      state.keyboardState.layout = termKeyboardLayout;
+                  }
+                  // ignore: invalid_use_of_protected_member
+                  state.setState(() {});
+                },
+                child: contents));
+      }
+    default:
+      {
+        print(
+            "ERROR: genParagraphItem(..): type '${item.type.name}' is not implemented");
+        return TextSpan(
+            text: "ERROR: genParagraphItem(..): "
+                "type '${item.type.name}' is not implemented",
+            style: TextStyle(color: Colors.red));
+      }
+  }
+}
