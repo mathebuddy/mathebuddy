40d8a8a420e20c45e6d7b3a495f5491058056ccb
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-09 17:42:18 +0100

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,21 +5,18 @@
 /// License: GPL-3.0-or-later
 
 import 'dart:convert';
-import 'dart:math';
 
-import 'package:tex/tex.dart';
+import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:universal_html/html.dart' as html;
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:mathebuddy/mbcl/src/chapter.dart';
 import 'package:mathebuddy/mbcl/src/course.dart';
-import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:mathebuddy/mbcl/src/level.dart';
 
 import 'package:mathebuddy/color.dart';
-import 'package:mathebuddy/keyboard_layouts.dart';
 import 'package:mathebuddy/keyboard.dart';
+import 'package:mathebuddy/level.dart';
 
 void main() {
   runApp(const MatheBuddy());
@@ -47,15 +44,22 @@ class CoursePage extends StatefulWidget {
   final String title;
 
   @override
-  State<CoursePage> createState() => _CoursePageState();
+  State<CoursePage> createState() => CoursePageState();
 }
 
-class _CoursePageState extends State<CoursePage> {
+class KeyboardState {
+  KeyboardLayout? layout; // null := not shown
+  MbclExerciseData? exerciseData;
+  MbclInputFieldData? inputFieldData;
+}
+
+class CoursePageState extends State<CoursePage> {
   String _courseData = '';
   MbclCourse? _course;
   MbclChapter? _chapter;
   MbclLevel? _level;
-  bool keyboardActive = false;
+
+  KeyboardState keyboardState = KeyboardState();
 
   @override
   void initState() {
@@ -73,7 +77,8 @@ class _CoursePageState extends State<CoursePage> {
           html.document.getElementById('course-data-span')?.innerHtml as String;
     } else {
       if (path == "DEBUG") path = "assets/hello_COMPILED.json";
-      _courseData = await DefaultAssetBundle.of(context).loadString(path);
+      _courseData =
+          await DefaultAssetBundle.of(context).loadString(path, cache: false);
     }
     print("received course: ");
     print(_courseData);
@@ -91,402 +96,14 @@ class _CoursePageState extends State<CoursePage> {
     setState(() {});
   }
 
-  InlineSpan _genParagraphItem(MbclLevelItem item,
-      {bold = false,
-      italic = false,
-      color = Colors.black,
-      MbclExerciseData? exerciseData}) {
-    double fontSize = 16;
-    switch (item.type) {
-      case MbclLevelItemType.reference:
-        {
-          return TextSpan(
-              text: "REFERENCES_NOT_YET_IMPLEMENTED",
-              style: TextStyle(color: Colors.red));
-        }
-      case MbclLevelItemType.text:
-        return TextSpan(
-          text: "${item.text} ",
-          style: TextStyle(
-              color: color,
-              fontSize: fontSize,
-              fontWeight: bold ? FontWeight.bold : FontWeight.normal,
-              fontStyle: italic ? FontStyle.italic : FontStyle.normal),
-        );
-      case MbclLevelItemType.boldText:
-      case MbclLevelItemType.italicText:
-      case MbclLevelItemType.color:
-        {
-          List<InlineSpan> gen = [];
-          switch (item.type) {
-            case MbclLevelItemType.boldText:
-              {
-                for (var it in item.items) {
-                  gen.add(_genParagraphItem(it,
-                      bold: true, exerciseData: exerciseData));
-                }
-                return TextSpan(children: gen);
-              }
-            case MbclLevelItemType.italicText:
-              {
-                for (var it in item.items) {
-                  gen.add(_genParagraphItem(it,
-                      italic: true, exerciseData: exerciseData));
-                }
-                return TextSpan(
-                  children: gen,
-                );
-              }
-            case MbclLevelItemType.color:
-              {
-                var colorKey = int.parse(item.id);
-                var colors = [
-                  // TODO
-                  Colors.black,
-                  Colors.red,
-                  Colors.blue,
-                  Colors.purple,
-                  Colors.orange
-                ];
-                var color = colors[colorKey % colors.length];
-                for (var it in item.items) {
-                  gen.add(_genParagraphItem(it,
-                      color: color, exerciseData: exerciseData));
-                }
-                return TextSpan(children: gen);
-              }
-            default:
-              // this will never happen
-              return TextSpan();
-          }
-        }
-      case MbclLevelItemType.inlineMath:
-        {
-          var tex = TeX();
-          var texSrc = '';
-          for (var subItem in item.items) {
-            switch (subItem.type) {
-              case MbclLevelItemType.text:
-                {
-                  texSrc += subItem.text;
-                  break;
-                }
-              case MbclLevelItemType.variableReference:
-                {
-                  var variableId = subItem.id;
-                  if (exerciseData == null) {
-                    texSrc += 'ERROR: not in exercise mode!';
-                  } else {
-                    var instance =
-                        exerciseData.instances[exerciseData.runInstanceIdx];
-                    var variableValue = instance[variableId];
-                    if (variableValue == null) {
-                      texSrc += 'ERROR: unknown exercise variable $variableId';
-                    } else {
-                      texSrc += variableValue;
-                    }
-                  }
-                  break;
-                }
-              default:
-                print(
-                    "ERROR: genParagraphItem(..): type '${item.type.name}' is not finally implemented");
-            }
-          }
-          tex.scalingFactor = 1.17;
-          var svg = tex.tex2svg(texSrc);
-          var svgWidth = tex.width;
-          if (svg.isEmpty) {
-            return TextSpan(
-              text: "${tex.error} ",
-              style: TextStyle(color: Colors.red, fontSize: fontSize),
-            );
-          } else {
-            return WidgetSpan(
-                alignment: PlaceholderAlignment.middle,
-                child: SvgPicture.string(svg, width: svgWidth.toDouble()));
-          }
-        }
-      case MbclLevelItemType.inputField:
-        {
-          return WidgetSpan(
-              alignment: PlaceholderAlignment.middle,
-              child: GestureDetector(
-                  onTap: () {
-                    keyboardActive = true;
-                    setState(() {});
-                  },
-                  child: Icon(
-                    Icons.keyboard,
-                    size: 42,
-                    color: matheBuddyRed,
-                  )));
-        }
-      default:
-        {
-          print(
-              "ERROR: genParagraphItem(..): type '${item.type.name}' is not implemented");
-          return TextSpan(
-              text: "ERROR: genParagraphItem(..): "
-                  "type '${item.type.name}' is not implemented",
-              style: TextStyle(color: Colors.red));
-        }
-    }
-  }
-
-  Widget _genLevelItem(MbclLevelItem item,
-      {paragraphPaddingLeft = 3.0,
-      paragraphPaddingRight = 3.0,
-      paragraphPaddingTop = 10.0,
-      paragraphPaddingBottom = 5.0,
-      MbclExerciseData? exerciseData}) {
-    switch (item.type) {
-      case MbclLevelItemType.section:
-        {
-          return Padding(
-              //padding: EdgeInsets.all(3.0),
-              padding: EdgeInsets.only(
-                  left: 3.0, right: 3.0, top: 10.0, bottom: 5.0),
-              child: Text(item.text,
-                  style: Theme.of(context).textTheme.headlineLarge));
-        }
-      case MbclLevelItemType.subSection:
-        {
-          return Padding(
-              padding: EdgeInsets.only(
-                  left: 3.0, right: 3.0, top: 10.0, bottom: 5.0),
-              child: Text(item.text,
-                  style: Theme.of(context).textTheme.headlineMedium));
-        }
-      case MbclLevelItemType.paragraph:
-        {
-          List<InlineSpan> list = [];
-          for (var subItem in item.items) {
-            list.add(_genParagraphItem(subItem, exerciseData: exerciseData));
-          }
-          var richText = RichText(
-            text: TextSpan(children: list),
-          );
-          return Padding(
-            padding: EdgeInsets.only(
-                left: paragraphPaddingLeft,
-                right: paragraphPaddingRight,
-                top: paragraphPaddingTop,
-                bottom: paragraphPaddingBottom),
-            child: richText,
-          );
-        }
-      case MbclLevelItemType.alignCenter:
-        {
-          List<Widget> list = [];
-          for (var subItem in item.items) {
-            list.add(_genLevelItem(subItem, exerciseData: exerciseData));
-          }
-          return Padding(
-              padding: EdgeInsets.all(3.0),
-              child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Wrap(alignment: WrapAlignment.start, children: list)));
-        }
-      case MbclLevelItemType.equation:
-        {
-          var texSrc = item.text;
-          Widget equationWidget = Text('');
-          var tex = TeX();
-          tex.scalingFactor = 1.1;
-          var svg = tex.tex2svg(texSrc);
-          var svgWidth = tex.width;
-          if (svg.isEmpty) {
-            equationWidget = Text(tex.error);
-          } else {
-            var eqNumber = int.parse(item.id);
-            var eqNumberWidget = Text(eqNumber >= 0 ? '($eqNumber)' : '');
-            equationWidget = Row(
-              children: [
-                Expanded(
-                    child: SvgPicture.string(svg, width: svgWidth.toDouble())),
-                Column(children: [eqNumberWidget]),
-              ],
-            );
-          }
-          return Padding(
-              padding: EdgeInsets.all(3.0),
-              child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Wrap(
-                      alignment: WrapAlignment.start,
-                      children: [equationWidget])));
-        }
-      case MbclLevelItemType.span:
-        {
-          List<InlineSpan> list = [];
-          for (var subItem in item.items) {
-            list.add(_genParagraphItem(subItem, exerciseData: exerciseData));
-          }
-          var richText = RichText(
-            text: TextSpan(children: list),
-          );
-          return Padding(
-            padding: EdgeInsets.all(3.0),
-            child: richText,
-          );
-        }
-      case MbclLevelItemType.itemize:
-      case MbclLevelItemType.enumerate:
-      case MbclLevelItemType.enumerateAlpha:
-        {
-          List<Row> rows = [];
-          for (var i = 0; i < item.items.length; i++) {
-            var subItem = item.items[i];
-            Widget w = Icon(
-              Icons.fiber_manual_record,
-              size: 8,
-            );
-            if (item.type == MbclLevelItemType.enumerate) {
-              w = Text("${i + 1}.");
-            } else if (item.type == MbclLevelItemType.enumerateAlpha) {
-              w = Text("${String.fromCharCode("a".codeUnitAt(0) + i)})");
-            }
-            var label = Column(children: [
-              Padding(
-                  padding: EdgeInsets.only(
-                      left: 15.0, right: 3.0, top: 0.0, bottom: 0.0),
-                  child: w)
-            ]);
-            var row = Row(children: [
-              label,
-              Column(children: [
-                _genLevelItem(subItem, exerciseData: exerciseData)
-              ])
-            ]);
-            rows.add(row);
-          }
-          return Column(children: rows);
-        }
-      case MbclLevelItemType.newPage:
-        {
-          return Text(
-            '\n--- page break will be here later ---\n',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          );
-        }
-      case MbclLevelItemType.defDefinition:
-      case MbclLevelItemType.defTheorem:
-        {
-          var prefix = '';
-          switch (item.type) {
-            case MbclLevelItemType.defDefinition:
-              prefix = 'Definition';
-              break;
-            case MbclLevelItemType.defTheorem:
-              prefix = 'Theorem';
-              break;
-            default:
-              prefix = 'UNIMPLEMENTED';
-              break;
-          }
-          List<Widget> list = [];
-          var title = Row(children: [
-            Padding(
-                padding: EdgeInsets.all(3.0),
-                child: Text('$prefix (${item.title})',
-                    style: TextStyle(fontWeight: FontWeight.bold)))
-          ]);
-          list.add(title);
-          for (var i = 0; i < item.items.length; i++) {
-            var subItem = item.items[i];
-            list.add(Wrap(children: [
-              _genLevelItem(subItem,
-                  paragraphPaddingLeft: 20.0,
-                  paragraphPaddingTop: i == 0 ? 0.0 : 10.0,
-                  exerciseData: exerciseData)
-            ]));
-          }
-          return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: list);
-        }
-      case MbclLevelItemType.exercise:
-        {
-          var data = item.exerciseData as MbclExerciseData;
-          if (data.runInstanceIdx < 0) {
-            data.runInstanceIdx = Random().nextInt(data.instances.length);
-          }
-          List<Widget> list = [];
-          var title = Wrap(children: [
-            Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Row(children: [
-                  Text(' '), // TODO: use padding instead of Text(' ')
-                  Icon(Icons.play_circle_outlined),
-                  Text(' '),
-                  // TODO: wrap does not work:
-                  Text(item.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
-                ]))
-          ]);
-          list.add(title);
-          for (var i = 0; i < item.items.length; i++) {
-            var subItem = item.items[i];
-            list.add(Wrap(children: [
-              _genLevelItem(subItem,
-                  paragraphPaddingLeft: 10.0,
-                  paragraphPaddingTop: i == 0 ? 5.0 : 10.0,
-                  exerciseData: item.exerciseData)
-            ]));
-          }
-          return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: list);
-        }
-      case MbclLevelItemType.multipleChoice:
-        {
-          List<Widget> mcOptions = [];
-          for (var i = 0; i < item.items.length; i++) {
-            var subItem = item.items[i];
-            mcOptions.add(_genLevelItem(subItem, exerciseData: exerciseData));
-          }
-          return Column(children: mcOptions);
-        }
-      case MbclLevelItemType.multipleChoiceOption:
-        {
-          var button = Column(children: [
-            Padding(
-                padding: EdgeInsets.only(
-                    left: 8.0, right: 2.0, top: 0.0, bottom: .0),
-                child: Icon(Icons.check_box_outline_blank,
-                    size: 36, color: matheBuddyRed)),
-          ]);
-          var text = Column(children: [
-            _genLevelItem(item.items[0], exerciseData: exerciseData)
-          ]);
-          return GestureDetector(
-              onTap: () {
-                print('pressed MC option');
-              },
-              child: Row(children: [button, text]));
-        }
-      default:
-        {
-          print(
-              "ERROR: genLevelItem(..): type '${item.type.name}' is not implemented");
-          return Text(
-            "\n--- ERROR: genLevelItem(..): type '${item.type.name}' is not implemented ---\n",
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
-          );
-        }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     Widget body = Text(
       "No course was selected!!!",
       style: Theme.of(context).textTheme.headlineLarge,
     );
+
+    var screenWidth = MediaQuery.of(context).size.width;
 
     List<Widget> page = [];
     if (_level == null) {
@@ -498,18 +115,46 @@ class _CoursePageState extends State<CoursePage> {
         'assets/typography_COMPILED.json',
         'DEBUG'
       ];
-      var listTiles = <Widget>[];
+      List<TableRow> tableRows = [];
       for (var course in courses) {
-        listTiles.add(ListTile(
-          enabled: true,
-          leading: Icon(Icons.fiber_manual_record),
-          title: Text(course),
-          onTap: () {
-            _selectCourse(course);
-          },
-        ));
+        tableRows.add(TableRow(children: [
+          TableCell(
+            child: GestureDetector(
+                onTap: () {
+                  _selectCourse(course);
+                },
+                child: Container(
+                    margin: EdgeInsets.all(2.0),
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 2.0, color: Color.fromARGB(255, 81, 81, 81)),
+                        borderRadius: BorderRadius.circular(100.0)),
+                    child: Center(child: Text(course)))),
+          )
+        ]));
       }
-      body = ListView(children: listTiles);
+
+      var coursesTable = Table(
+        //border: TableBorder.all(),
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: tableRows,
+      );
+
+      var logo =
+          Column(children: [Image.asset('assets/img/logo-large-en.png')]);
+      var contents = Column(children: [logo, coursesTable]);
+
+      body = SingleChildScrollView(padding: EdgeInsets.all(5), child: contents);
+      /*Container(
+              //color: Colors.white,
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(left: 3.0, right: 3.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: rows,
+              ))*/
     } else {
       var level = _level as MbclLevel;
       page.add(Padding(
@@ -523,7 +168,7 @@ class _CoursePageState extends State<CoursePage> {
                 fontWeight: FontWeight.bold),
           )));
       for (var item in level.items) {
-        page.add(_genLevelItem(item));
+        page.add(generateLevelItem(this, item));
       }
       // add empty lines at the end; otherwise keyboard is in the way...
       for (var i = 0; i < 10; i++) {
@@ -549,16 +194,15 @@ class _CoursePageState extends State<CoursePage> {
     ));*/
 
     var keyboard = Keyboard();
-    var screenWidth = MediaQuery.of(context).size.width;
     //print('screen width = $screenWidth');
 
     Widget bottomArea = Text('');
-    if (keyboardActive) {
+    if (keyboardState.layout != null) {
       bottomArea = Container(
           color: Colors.black26,
           alignment: Alignment.bottomCenter,
           constraints: BoxConstraints(maxHeight: 285.0),
-          child: keyboard.generateWidget(integerKeyboardLayout, screenWidth));
+          child: keyboard.generateWidget(this, keyboardState, screenWidth));
     }
 
     return Scaffold(
@@ -622,7 +266,7 @@ class _CoursePageState extends State<CoursePage> {
           IconButton(
             onPressed: () {
               _level = null;
-              keyboardActive = false;
+              keyboardState.layout = null;
               setState(() {});
             },
             icon: Icon(Icons.home, size: 36),
