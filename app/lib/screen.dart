0f7168a1ffd55913898ce92284fa54cb2dd73ded
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-14 16:26:01 +0100

    Create screen.dart

---
+++ app/lib/screen.dart
@@ -0,0 +1,7 @@
+/// mathe:buddy - a gamified app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+double screenWidth = 0.0;
