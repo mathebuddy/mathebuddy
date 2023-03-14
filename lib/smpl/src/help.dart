9a58ad4d601238086239d20caaa2b227f2b6c93f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-14 16:25:20 +0100

    Create help.dart

---
+++ lib/smpl/src/help.dart
@@ -0,0 +1,13 @@
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+String spaces(int n) {
+  var s = '';
+  for (var i = 0; i < n; i++) {
+    s += '  ';
+  }
+  return s;
+}
