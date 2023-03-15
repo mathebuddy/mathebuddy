0e6e05a9aa766d5ba399fd394b6c68ba210538a2
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-15 18:22:56 +0100

    Create help.dart

---
+++ app/lib/help.dart
@@ -0,0 +1,32 @@
+/// mathe:buddy - a gamified app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+import 'dart:math';
+
+void shuffleIntegerList(List<int> list) {
+  var n = list.length;
+  for (var k = 0; k < n; k++) {
+    var i = Random().nextInt(n);
+    var j = Random().nextInt(n);
+    var tmp = list[i];
+    list[i] = list[j];
+    list[j] = tmp;
+  }
+}
+
+String convertMath2TeX(String s) {
+  // example:
+  //   '[[2,4],[1,5]]'
+  // is converted to
+  //   '\begin{pmatrix} 2 & 4 \\ 1 & 5 \\ \end{pmatrix}
+  if (s.startsWith('[[')) {
+    s = s.replaceAll('[[', '\\begin{pmatrix}');
+    s = s.replaceAll(']]', '\\end{pmatrix}');
+    s = s.replaceAll('],[', ' \\\\');
+    s = s.replaceAll(',', '&');
+  }
+  return s;
+}
