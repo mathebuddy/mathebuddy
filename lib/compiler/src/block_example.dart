76ec3d31bf980de12e7c4587d63b9d7d45e46145
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-17 16:22:54 +0100

    Create block_example.dart

---
+++ lib/compiler/src/block_example.dart
@@ -0,0 +1,27 @@
+/// mathe:buddy - a gamified learning-app for higher math
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+import '../../mbcl/src/level_item.dart';
+
+import 'block.dart';
+
+MbclLevelItem processExample(Block block) {
+  var example = MbclLevelItem(MbclLevelItemType.example);
+  example.title = block.title;
+  example.label = block.label;
+  for (var part in block.parts) {
+    switch (part.name) {
+      case 'global':
+        example.items
+            .addAll(block.compiler.parseParagraph(part.lines.join("\n")));
+        break;
+      default:
+        example.error += 'Unexpected part "${part.name}".';
+    }
+  }
+  block.processSubblocks(example);
+  return example;
+}
