7525f6a6f087c8f6d2515bcfa9b2891f8f6b7221
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-17 16:23:17 +0100

    Update keyboard_layouts.dart

--- app/lib/keyboard_layouts.dart
+++ app/lib/keyboard_layouts.dart
@@ -9,56 +9,56 @@ import 'package:mathebuddy/keyboard.dart';
 /// !B := backspace
 /// !E := enter
 
-var integerKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutInteger = KeyboardLayout.parse('''
 7 8 9 !B
 4 5 6 !B
 1 2 3 !E
 0 0 - !E
 ''');
 
-var integerOpKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutIntegerWithOperators = KeyboardLayout.parse('''
 7 8 9 + !B
 4 5 6 - !B
 1 2 3 * !E
 0 0 0 / !E
 ''');
 
-var realNumberKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutReal = KeyboardLayout.parse('''
 7 8 9 !B
 4 5 6 !B
 1 2 3 !E
 0 . - !E
 ''');
 
-var realNumberOpKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutRealWithOperators = KeyboardLayout.parse('''
 7 8 9 + !B
 4 5 6 - !B
 1 2 3 * !E
 0 0 . / !E
 ''');
 
-var complexNormalFormKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutComplexNormalForm = KeyboardLayout.parse('''
 7 8 9 + !B
 4 5 6 - !B
 1 2 3 i !E
 0 0 0 . !E
 ''');
 
-var integerSetKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutIntegerSet = KeyboardLayout.parse('''
 7 8 9 { !B
 4 5 6 } !B
 1 2 3 - !E
 0 0 0 , !E
 ''');
 
-var complexSetKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutComplexIntegerSet = KeyboardLayout.parse('''
 7 8 9 { !B
 4 5 6 } !B
 1 2 3 - !E
 0 0 i , !E
 ''');
 
-var termKeyboardLayout = KeyboardLayout.parse('''
+var keyboardLayoutTerm = KeyboardLayout.parse('''
 7 8 9 + sin( (   !B
 4 5 6 - cos( )   !B
 1 2 3 * tan( pi  !E
