9618423c9767047a2570dd9156c6dac50d17135b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-17 12:31:22 +0100

    Update index.dart

--- docs/src/index.dart
+++ docs/src/index.dart
@@ -4,18 +4,18 @@
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
 
+import 'chat_playground.dart' as chat_play;
 import 'math_runtime_playground.dart' as math_play;
-import 'smpl_playground.dart' as smpl_play;
 import 'mbl_playground.dart' as mbl_play;
-import 'tex_playground.dart' as tex_play;
 import 'sim.dart' as sim;
+import 'smpl_playground.dart' as smpl_play;
+import 'tex_playground.dart' as tex_play;
 
 void main() async {
   math_play.mathRuntimePlayground();
   smpl_play.smplPlayground();
   mbl_play.mblPlayground();
   tex_play.texPlayground();
+  chat_play.chatPlayground();
   sim.init();
-  //var files = await getFilesFromDir('demo/');
-  //print(files.toString());
 }
