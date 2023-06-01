e2eade294f4ae19be115578f309b3bbde164a584
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-06-01 13:46:27 +0200

    Update block_table.dart

--- lib/compiler/src/block_table.dart
+++ lib/compiler/src/block_table.dart
@@ -23,8 +23,9 @@ MbclLevelItem processTable(Block block) {
     switch (part.name) {
       case 'global':
         if (part.lines.join('\n').trim().isNotEmpty) {
-          table.error += 'Some of your code is not inside a tag'
-              ' (e.g. "@code" or "@text").';
+          table.error += 'Some of your code '
+              '("${part.lines.join('\n').trim().substring(0, 10)}...")'
+              ' is not inside a tag (e.g. "@code" or "@text").';
         }
         break;
       case 'options':
