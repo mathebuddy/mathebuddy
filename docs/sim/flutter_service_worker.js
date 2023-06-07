9394151f6e38a8fcf3979e6c4c0e04951d0b5068
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-06-07 11:22:27 +0200

    web rebuild

--- docs/sim/flutter_service_worker.js
+++ docs/sim/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "9cf758d2982da256e3254de383e2c8b6",
-"index.html": "d56a56aca16ad157e1fa48982efd1270",
-"/": "d56a56aca16ad157e1fa48982efd1270",
-"main.dart.js": "15470eae9dbbee11b85a64695d4a4710",
+  "version.json": "f6b1e481994b799ace9d51b8fe335e52",
+"index.html": "9c86aa7840342d5a8ac82f558518b5c5",
+"/": "9c86aa7840342d5a8ac82f558518b5c5",
+"main.dart.js": "696effa7ae5b74444c1b18237c67c648",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "85964310ed8019418c7134094b6e63ac",
 "logo.png": "5244282b4556da033d86dd0c8d14bd49",
@@ -21,7 +21,7 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/RobotoMono-Regular.ttf": "7e173cf37bb8221ac504ceab2acfb195",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/assets/bundle-test.json": "7129d48a0f61f42da0d91ac6101ffe80",
+"assets/assets/bundle-test.json": "d450d0de2751774b5b46f19288738393",
 "assets/assets/bundle-alpha.json": "dac6c02bff735594bb919e1dd6a5bf21",
 "assets/assets/img/logoSmall.png": "82b9a45f27b91939b785cfba21a32f2a",
 "assets/assets/img/logo-large-en.png": "5b4cba2e3ba850ec9039165ac7ba7e14",
