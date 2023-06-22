976f01cf3df04ea804a4ada52c4e1c7003f70d64
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-06-22 21:18:41 +0200

    Update str.dart

--- lib/math-runtime/src/str.dart
+++ lib/math-runtime/src/str.dart
@@ -55,3 +55,65 @@ String term2string(Term term) {
   }
   return s;
 }
+
+String term2tex(Term term) {
+  var s = '';
+  switch (term.op) {
+    case '#':
+    case '\$':
+      if (term.value.type == OperandType.int ||
+          term.value.type == OperandType.rational ||
+          term.value.type == OperandType.real ||
+          term.value.type == OperandType.irrational ||
+          term.value.type == OperandType.identifier) {
+        s = term.value.toTeXString();
+      } else {
+        s = '(${term.value})';
+      }
+      break;
+    case '.-':
+      s += '(-(${term.o[0]}))'; // TODO: test!!
+      break;
+    default:
+      if (term.op.length > 2) {
+        // sin, cos, tan, exp, ... TODO!!
+        if (term.op == 'sin' ||
+            term.op == 'cos' ||
+            term.op == 'tan' ||
+            term.op == 'exp') {
+          s += '\\';
+        }
+        s += term.op;
+        if (term.dims.isNotEmpty) {
+          s += '<';
+          for (var i = 0; i < term.dims.length; i++) {
+            if (i > 0) s += ',';
+            s += term.dims[i].toTeXString();
+          }
+          s += '>';
+        }
+        s += '\\left(';
+        for (var i = 0; i < term.o.length; i++) {
+          if (i > 0) s += ',';
+          s += term.o[i].toTeXString();
+        }
+        s += '\\right)';
+      } else {
+        // '+', '-', ...
+        s += '(';
+        for (var i = 0; i < term.o.length; i++) {
+          if (i > 0) {
+            if (term.op == '*') {
+              s += '\\cdot';
+            } else {
+              s += term.op;
+            }
+          }
+          s += term.o[i].toTeXString();
+        }
+        s += ')';
+      }
+      break;
+  }
+  return s;
+}
