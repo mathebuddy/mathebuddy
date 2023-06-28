41dd5584e4926dadeffb94dd0c13ab9b247e15e8
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-06-28 13:51:08 +0200

    rebuilt web version

--- docs/sim/flutter_service_worker.js
+++ docs/sim/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f6b1e481994b799ace9d51b8fe335e52",
-"index.html": "cda9f97265021b846f43aee7b782e752",
-"/": "cda9f97265021b846f43aee7b782e752",
-"main.dart.js": "584dd1d6c5c619f5095f6b8180457240",
+"index.html": "efd0b8ff9e9022df0bc6ef415f755841",
+"/": "efd0b8ff9e9022df0bc6ef415f755841",
+"main.dart.js": "cb8a1751087e3c1e5caa0d2118150bd3",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "85964310ed8019418c7134094b6e63ac",
 "logo.png": "5244282b4556da033d86dd0c8d14bd49",
@@ -28,7 +28,7 @@ const RESOURCES = {
 "assets/assets/img/logo-large-en.png": "5b4cba2e3ba850ec9039165ac7ba7e14",
 "assets/assets/img/logo-large-de.png": "eb1ba8c2c4d04ea072c4c8c2239bd950",
 "assets/assets/img/logo.png": "5244282b4556da033d86dd0c8d14bd49",
-"assets/assets/bundle-complex.json": "3ba5fba29001b38d2f8c1e3cee56567a",
+"assets/assets/bundle-complex.json": "8065da78718377d8fdf3e4e8e901db1e",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
