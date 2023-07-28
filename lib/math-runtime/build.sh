27b7f2fb13445bbd477a05a4c6acddbb1c0564b6
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-07-28 13:55:55 +0200

    Update build.sh

--- lib/math-runtime/build.sh
+++ lib/math-runtime/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "# extracted automatically by running ./build.sh" > grammar.txt
-cat src/parse.dart | grep //G | cut -c7- >> grammar.txt
+#echo "# extracted automatically by running ./build.sh" > grammar.txt
+#cat src/parse.dart | grep //G | cut -c7- >> grammar.txt
+python3 build.py
