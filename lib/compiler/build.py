025d4a97c8eac6855ba49d483ef286af448b7096
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-07-28 13:55:42 +0200

    Create build.py

---
+++ lib/compiler/build.py
@@ -0,0 +1,31 @@
+# mathe:buddy - a gamified learning-app for higher math
+# (c) 2022-2023 by TH Koeln
+# Author: Andreas Schwenk contact@compiler-construction.com
+# Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+# License: GPL-3.0-or-later
+
+grammar = ""
+
+paths = ["src/level.dart", "src/paragraph.dart", "src/math.dart"]
+
+for path in paths:
+    f = open(path, "r")
+    lines = f.readlines()
+    f.close()
+    recording = False
+    for line in lines:
+        if line.startswith("///") is False:
+            continue
+        line = line[4:]
+        if line.startswith("<GRAMMAR"):
+            recording = True
+        if recording:
+            grammar += line
+        if line.startswith("</GRAMMAR"):
+            recording = False
+
+f = open("grammar.txt", "w")
+f.writelines(grammar)
+f.close()
+
+# print(grammar)
