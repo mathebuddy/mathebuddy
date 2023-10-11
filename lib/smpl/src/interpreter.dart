98df0efdee14d2b3255b2dcd631ea516eeb78ab2
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-10-11 16:45:05 +0200

    Update interpreter.dart

--- lib/smpl/src/interpreter.dart
+++ lib/smpl/src/interpreter.dart
@@ -44,143 +44,257 @@ class Interpreter {
 
   void _run(AstNode node) {
     if (node is StatementList) {
-      // ----- statement list -----
-      var statementList = node;
-      if (statementList.createScope) _symbolTable.add({}); // open a new scope
-      for (var i = 0; i < statementList.statements.length; i++) {
-        var statement = statementList.statements[i];
-        _run(statement);
-      }
-      if (statementList.createScope) _symbolTable.removeLast(); // close scope
+      _statementList(node);
     } else if (node is Assignment) {
-      // ----- assignment -----
-      var assignment = node;
-      InterpreterSymbol? symbol;
-      if (assignment.createSymbol) {
-        symbol = _addSymbol(assignment.lhs, assignment.row);
-      } else {
-        symbol = _getSymbol(assignment.lhs);
-      }
-      if (symbol == null) {
-        _error(node.row, 'symbol "${assignment.lhs}" is unknown.');
-      } else {
-        symbol.term =
-            _processTerm(assignment.rhs, assignment.vars, assignment.row);
-        if (assignment.vars.isEmpty) {
-          var isOK = true;
-          var k = 0;
-          do {
+      _assignment(node);
+    } else if (node is WhileLoop) {
+      _whileLoop(node);
+    } else if (node is DoWhileLoop) {
+      _doWhileLoop(node);
+    } else if (node is ForLoop) {
+      _forLoop(node);
+    } else if (node is IfCond) {
+      _ifCondition(node);
+    } else if (node is Figure) {
+      _figure(node);
+    } else if (node is Print) {
+      _print(node);
+    } else {
+      throw Exception('unimplemented AstNode type ${node.runtimeType}.');
+    }
+  }
+
+  void _statementList(StatementList sl) {
+    if (sl.createScope) _symbolTable.add({}); // open a new scope
+    for (var i = 0; i < sl.statements.length; i++) {
+      var statement = sl.statements[i];
+      _run(statement);
+    }
+    if (sl.createScope) _symbolTable.removeLast(); // close scope
+  }
+
+  void _assignment(Assignment assignment) {
+    InterpreterSymbol? symbol;
+    if (assignment.createSymbol) {
+      symbol = _addSymbol(assignment.lhs, assignment.row);
+    } else {
+      symbol = _getSymbol(assignment.lhs);
+    }
+    if (symbol == null) {
+      _error(assignment.row, 'symbol "${assignment.lhs}" is unknown.');
+    } else {
+      var oldTerm = symbol.term;
+      var newTerm =
+          _processTerm(assignment.rhs, assignment.vars, assignment.row);
+      symbol.term = newTerm;
+      if (assignment.vars.isEmpty) {
+        var isOK = true;
+        var k = 0;
+        do {
+          if (assignment.lhsIndex1.isEmpty) {
             symbol.value = symbol.term.eval({}); // TODO: error handling
-            isOK = true;
-            for (var i = 0; i < assignment.independentTo.length; i++) {
-              var indTo = assignment.independentTo[i];
-              var s2 = _getSymbol(indTo);
-              if (Operand.compareEqual(symbol.value, s2?.value as Operand)) {
-                isOK = false;
-                break;
+          } else {
+            var index1Term = _processTerm(
+                assignment.lhsIndex1, assignment.vars, assignment.row);
+            var index1Operand = index1Term.eval({});
+            var index1Value = index1Operand.real.toInt();
+            if (assignment.lhsIndex2.isNotEmpty) {
+              var index2Term = _processTerm(
+                  assignment.lhsIndex2, assignment.vars, assignment.row);
+              var index2Operand = index2Term.eval({});
+              var index2Value = index2Operand.real.toInt();
+              // 2-dim index
+              if (symbol.value.type != OperandType.matrix) {
+                _error(assignment.row, 'only matrices can be 2-dim indexed.');
+              }
+              if (index1Value < 0 || index1Value >= symbol.value.rows) {
+                _error(assignment.row, 'index value $index1Value is invalid.');
+              }
+              if (index2Value < 0 || index2Value >= symbol.value.cols) {
+                _error(assignment.row, 'index value $index2Value is invalid.');
+              }
+              symbol.value
+                      .items[index1Value * symbol.value.cols + index2Value] =
+                  symbol.term.eval({});
+              if (oldTerm.op == "matrix") {
+                symbol.term = oldTerm;
+                symbol.term.o[index1Value * symbol.value.cols + index2Value] =
+                    newTerm;
+              } else {
+                // hack (e.g. for "zeros")
+                symbol.term = Term.createConst(symbol.value);
+              }
+            } else {
+              // 1-dim index
+              if (symbol.value.type != OperandType.vector) {
+                _error(assignment.row, 'only vectors can be indexed.');
+              }
+              if (index1Value < 0 || index1Value >= symbol.value.items.length) {
+                _error(assignment.row, 'index value $index1Value is invalid.');
+              }
+              symbol.value.items[index1Value] = symbol.term.eval({});
+              if (oldTerm.op == "vec") {
+                symbol.term = oldTerm;
+                symbol.term.o[index1Value] = newTerm;
+              } else {
+                // hack (e.g. for "zeros")
+                symbol.term = Term.createConst(symbol.value);
               }
             }
-            k++;
-            if (k >= 50) {
-              _error(
-                assignment.row,
-                'failed to create different variable values for "${symbol.id}"',
-              );
+          }
+          isOK = true;
+          for (var i = 0; i < assignment.independentTo.length; i++) {
+            var indTo = assignment.independentTo[i];
+            var s2 = _getSymbol(indTo);
+            if (Operand.compareEqual(symbol.value, s2?.value as Operand)) {
+              isOK = false;
+              break;
             }
-          } while (isOK == false);
-          if (_verbose) {
-            print(
-              '>> assigned ${symbol.id}'
-              ' := ${symbol.value} (${symbol.value.type.name})',
+          }
+          k++;
+          if (k >= 50) {
+            _error(
+              assignment.row,
+              'failed to create different variable values for "${symbol.id}".',
             );
           }
-          if (assignment.expectedRhs.isNotEmpty) {
-            var expectedType = OperandType.int;
-            try {
-              expectedType = OperandType.values.byName(assignment.expectedType);
-            } catch (e) {
-              _error(assignment.row,
-                  'unknown/unimplemented type "${assignment.expectedType}".');
-            }
-            if (symbol.value.type != expectedType) {
+        } while (isOK == false);
+        if (_verbose) {
+          print(
+            '>> assigned ${symbol.id}'
+            ' := ${symbol.value} (${symbol.value.type.name})',
+          );
+        }
+        if (assignment.expectedRhs.isNotEmpty) {
+          var expectedType = OperandType.int;
+          try {
+            expectedType = OperandType.values.byName(assignment.expectedType);
+          } catch (e) {
+            _error(assignment.row,
+                'unknown/unimplemented type "${assignment.expectedType}".');
+          }
+          if (symbol.value.type != expectedType) {
+            _error(
+                assignment.row,
+                'TEST FAILED: expected type "${expectedType.name}",'
+                ' but got type "${symbol.value.type.name}".');
+          }
+          Operand? expectedValue;
+          try {
+            var expectedTerm = _termParser.parse(assignment.expectedRhs,
+                splitIdentifiers: false);
+            expectedValue = expectedTerm.eval({});
+          } catch (e) {
+            _error(assignment.row,
+                'error in expected-term "$assignment.expectedRhs":$e.');
+          }
+          if (Operand.compareEqual(expectedValue!, symbol.value) == false) {
+            _error(
+                assignment.row,
+                'expected value "${expectedValue.toString()}",'
+                ' but got value "${symbol.value.toString()}".');
+          }
+          if (assignment.expectedStringifiedTerm.isNotEmpty) {
+            var expected = assignment.expectedStringifiedTerm;
+            var actual = symbol.term.toString();
+            if (expected.replaceAll(' ', '') != actual.replaceAll(' ', '')) {
               _error(
                   assignment.row,
-                  'TEST FAILED: expected type "${expectedType.name}",'
-                  ' but got type "${symbol.value.type.name}".');
-            }
-            Operand? expectedValue;
-            try {
-              var expectedTerm = _termParser.parse(assignment.expectedRhs,
-                  splitIdentifiers: false);
-              expectedValue = expectedTerm.eval({});
-            } catch (e) {
-              _error(assignment.row,
-                  'error in expected-term "$assignment.expectedRhs":$e.');
-            }
-            if (Operand.compareEqual(expectedValue!, symbol.value) == false) {
-              _error(
-                  assignment.row,
-                  'expected value "${expectedValue.toString()}",'
-                  ' but got value "${symbol.value.toString()}".');
-            }
-            if (assignment.expectedStringifiedTerm.isNotEmpty) {
-              var expected = assignment.expectedStringifiedTerm;
-              var actual = symbol.term.toString();
-              if (expected.replaceAll(' ', '') != actual.replaceAll(' ', '')) {
-                _error(
-                    assignment.row,
-                    'expected stringified term "$expected",'
-                    ' but got "$actual"');
-              }
+                  'expected stringified term "$expected",'
+                  ' but got "$actual"');
             }
           }
         }
       }
-    } else if (node is WhileLoop) {
-      // ----- while loop -----
-      var whileLoop = node;
-      for (;;) {
-        var condition = _processTerm(whileLoop.condition, [], whileLoop.row)
-            .eval({}); // TODO: error handling
-        if (condition.type != OperandType.boolean) {
-          _error(whileLoop.row, 'while-loop expects a boolean condition');
-        }
-        if (condition.real == 0 /* 0 := false */) break;
-        _run(whileLoop.statements as StatementList);
-      }
-    } else if (node is IfCond) {
-      // ----- if condition -----
-      var ifCond = node;
-      var condition = _processTerm(ifCond.condition, [], ifCond.row)
+    }
+  }
+
+  void _whileLoop(WhileLoop whileLoop) {
+    for (;;) {
+      var condition = _processTerm(whileLoop.condition, [], whileLoop.row)
           .eval({}); // TODO: error handling
       if (condition.type != OperandType.boolean) {
-        _error(ifCond.row, 'if-statement expects a boolean condition');
+        _error(whileLoop.row, 'while-loop expects a boolean condition.');
+      }
+      if (condition.real == 0 /* 0 := false */) break;
+      _run(whileLoop.statements as StatementList);
+    }
+  }
+
+  void _doWhileLoop(DoWhileLoop doWhileLoop) {
+    for (;;) {
+      _run(doWhileLoop.statements as StatementList);
+      var condition = _processTerm(doWhileLoop.condition, [], doWhileLoop.row)
+          .eval({}); // TODO: error handling
+      if (condition.type != OperandType.boolean) {
+        _error(doWhileLoop.row, 'while-loop expects a boolean condition.');
+      }
+      if (condition.real == 0 /* 0 := false */) break;
+    }
+  }
+
+  void _forLoop(ForLoop forLoop) {
+    // TODO: error handlingde;
+    var lowerBound = _processTerm(forLoop.lowerBound, [], forLoop.row).eval({});
+    if (lowerBound.type != OperandType.int) {
+      _error(forLoop.row, 'lower bound in for-loop must be integral.');
+    }
+    var upperBound = _processTerm(forLoop.upperBound, [], forLoop.row).eval({});
+    if (upperBound.type != OperandType.int) {
+      _error(forLoop.row, 'upper bound in for-loop must be integral.');
+    }
+    if (upperBound.real - lowerBound.real > 1e6) {
+      _error(forLoop.row, 'for-loop range is too large.');
+    }
+    var loopVar = _addSymbol(forLoop.variableId, forLoop.row);
+    loopVar.term = Term.createConstInt(lowerBound.real);
+    loopVar.value = loopVar.term.eval({});
+    for (;;) {
+      if (loopVar.value.real > upperBound.real) {
+        break;
+      }
+      _run(forLoop.statements as StatementList);
+      loopVar.value.real++;
+      loopVar.term = Term.createConstInt(loopVar.value.real);
+    }
+    // TODO: remove loopVar from symbol table after loop!
+  }
+
+  void _ifCondition(IfCond ifCond) {
+    for (var k = 0; k < ifCond.conditions.length; k++) {
+      var condSrc = ifCond.conditions[k];
+      var condition = _processTerm(condSrc, [], ifCond.row)
+          .eval({}); // TODO: error handling
+      if (condition.type != OperandType.boolean) {
+        _error(ifCond.row, 'if-statement expects a boolean conditions.');
       }
       if (condition.real != 0) {
-        _run(ifCond.statementsTrue as StatementList);
-      } else if (ifCond.statementsFalse != null) {
-        _run(ifCond.statementsFalse as StatementList);
+        _run(ifCond.blocks[k]);
+        break;
       }
-    } else if (node is Figure) {
-      // ----- figure -----
-      var figure = node;
-      var functionIDs = figure.getReferencesFunctionIDs();
-      Map<String, InterpreterSymbol> functionSymbols = {};
-      for (var functionID in functionIDs) {
-        var symbol = _getSymbol(functionID);
-        if (symbol == null) {
-          _error(figure.row, 'figure accesses unknown function $functionID');
-        } else {
-          functionSymbols[functionID] = symbol;
-        }
-      }
-      InterpreterSymbol? figureSymbol;
-      figureSymbol = _addSymbol('__figure', figure.row);
-      var svg = figure.generateSVG(functionSymbols);
-      figureSymbol.value = Operand.createString(svg);
-    } else {
-      throw Exception('unimplemented AstNode type ${node.runtimeType}');
     }
+  }
+
+  void _figure(Figure figure) {
+    var functionIDs = figure.getReferencesFunctionIDs();
+    Map<String, InterpreterSymbol> functionSymbols = {};
+    for (var functionID in functionIDs) {
+      var symbol = _getSymbol(functionID);
+      if (symbol == null) {
+        _error(figure.row, 'figure accesses unknown function $functionID.');
+      } else {
+        functionSymbols[functionID] = symbol;
+      }
+    }
+    InterpreterSymbol? figureSymbol;
+    figureSymbol = _addSymbol('__figure', figure.row);
+    var svg = figure.generateSVG(functionSymbols);
+    figureSymbol.value = Operand.createString(svg);
+  }
+
+  void _print(Print p) {
+    var term = _processTerm(p.term, [], p.row).eval({}); // TODO: error handling
+    print(">>> $term");
   }
 
   InterpreterSymbol _addSymbol(String id, int srcRow) {
