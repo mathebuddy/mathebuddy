3ff9f0682ba7cffc8e828667afb6860c68d4640c
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-10-17 15:21:40 +0200

    Update level_paragraph_item_math.dart

--- app/lib/level_paragraph_item_math.dart
+++ app/lib/level_paragraph_item_math.dart
@@ -66,7 +66,7 @@ InlineSpan generateParagraphItemMath(LevelState state, MbclLevelItem item,
       displayStyle: item.type == MbclLevelItemType.displayMath,
       deltaYOffset: -110);
   var svgWidth = tex.width;
-  if (svg.isEmpty) {
+  if (tex.success()) {
     return TextSpan(
       text: "${tex.error}. TEX-INPUT: $texSrc",
       style: TextStyle(color: Colors.red, fontSize: defaultFontSize),
