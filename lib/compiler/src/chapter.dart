806b6a296c6321911e41dbefc5ea2ad3161e764a
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-03 14:57:53 +0100

    Update chapter.dart

--- lib/compiler/src/chapter.dart
+++ lib/compiler/src/chapter.dart
@@ -5,12 +5,25 @@
 /// License: GPL-3.0-or-later
 
 import '../../mbcl/src/chapter.dart';
+import '../../mbcl/src/level_item.dart';
 
 import 'level.dart';
 
 void postProcessChapter(MbclChapter chapter) {
+  List<MbclLevelItem> exercises = [];
   for (var i = 0; i < chapter.levels.length; i++) {
     var level = chapter.levels[i];
     postProcessLevel(level);
+    // gather exercises
+    var levelExercises = level.getExercises();
+    exercises.addAll(levelExercises);
+  }
+  // check, if all exercises have distinct labels. If not, generate error
+  Set<String> exerciseLabels = {};
+  for (var ex in exercises) {
+    if (exerciseLabels.contains(ex.label)) {
+      chapter.error += ' Exercise label "${ex.label}" is given twice or more. ';
+    }
+    exerciseLabels.add(ex.label);
   }
 }
