682b4795ced9416489b0ae0419b69ab7c1586d92
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-03 14:57:41 +0100

    Update input.dart

--- lib/compiler/src/input.dart
+++ lib/compiler/src/input.dart
@@ -7,6 +7,8 @@
 import 'package:slex/slex.dart';
 
 import '../../math-runtime/src/operand.dart';
+import '../../math-runtime/src/parse.dart';
+import '../../math-runtime/src/term.dart';
 
 import '../../mbcl/src/level.dart';
 import '../../mbcl/src/level_item.dart';
@@ -27,7 +29,7 @@ import 'help.dart';
 ///     | "ROWS" "=" ("static"|"dynamic")   % default is static
 ///     | "COLS" "=" ("static"|"dynamic")   % default is static
 ///     | "KEYBOARD" "=" ID                 % force input keyboard
-///     | "CHOICES" "=" INT                 % provides N solutions to select
+///     | "CHOICES" "=" INT { "+" STR }     % provides N solutions to select
 ///                                         %   -1 := no choices
 ///     | "TOKENS" REAL { "+" STR }         % term-tokens
 ///     | "HIDE_LENGTH"            % length is shown per default
@@ -48,8 +50,14 @@ MbclLevelItem parseInputElement(
   // reference to a variable from part CODE
   if (lexer.isIdentifier()) {
     data.variableId = lexer.identifier();
+    // check if variable exists
+    if (exerciseData.variables.contains(data.variableId) == false) {
+      exercise.error +=
+          ' Input field uses unknown variable "${data.variableId}". ';
+      return inputField;
+    }
   }
-  // explicit string -> gap question
+  // string -> gap question
   else if (lexer.isString()) {
     isGap = true;
     var gapString = lexer.string();
@@ -58,100 +66,6 @@ MbclLevelItem parseInputElement(
     data.type = MbclInputFieldType.string;
   } else {
     exercise.error += ' No variable for input field given. ';
-    return inputField;
-  }
-  // optional attributes e.g. ",SCORE=", ",DIFF=x", ...
-  Map<String, String> attributes = {};
-  while (lexer.isTerminal(",")) {
-    lexer.next();
-    var key = lexer.getToken().token.trim();
-    lexer.next();
-    var value = "";
-    if (lexer.isTerminal("=")) {
-      lexer.next();
-      value = lexer.getToken().token.trim();
-      lexer.next();
-      while (lexer.isTerminal("+")) {
-        value += "+";
-        lexer.next();
-        value += lexer.getToken().token.trim(); // TODO: "+" could be within STR
-        lexer.next();
-      }
-    }
-    if (value.isEmpty) value = "true";
-    attributes[key] = value;
-  }
-  try {
-    // TODO: check semantics!!
-    checkAttributes(attributes, [
-      "DIFF",
-      "SCORE",
-      "ARRANGE",
-      "ROWS",
-      "COLS",
-      "KEYBOARD",
-      "CHOICES",
-      "TOKENS",
-      "HIDE_LENGTH",
-      "SHOW_ALL_LETTERS",
-    ]);
-    data.diffVariableId = "";
-    if (hasAttribute(attributes, "DIFF")) {
-      data.diffVariableId = getAttributeIdentifier(attributes, "DIFF");
-    }
-    data.score = getAttributeInt(attributes, "SCORE", 1);
-    data.arrange = getAttributeBool(attributes, "ARRANGE", false);
-    data.dynamicRows = getAttributeString(
-            attributes, "ROWS", ["dynamic", "static"], "static") ==
-        "dynamic";
-    data.dynamicCols = getAttributeString(
-            attributes, "COLS", ["dynamic", "static"], "static") ==
-        "dynamic";
-    data.forceKeyboardId = getAttributeIdentifier(attributes, "KEYBOARD");
-    data.numChoices = getAttributeInt(attributes, "CHOICES", -1);
-    // TODO: var tokens = ;
-    data.hideLengthOfGap = getAttributeBool(attributes, "HIDE_LENGTH", false);
-    data.showAllLettersOfGap =
-        getAttributeBool(attributes, "SHOW_ALL_LETTERS", false);
-  } catch (e) {
-    exercise.error += e.toString();
-  }
-  // TODO: remove old src
-  // while (lexer.isTerminal(",")) {
-  //   var key = "", value = "";
-  //   lexer.next();
-  //   key = lexer.getToken().token;
-  //   lexer.next();
-  //   if (lexer.isTerminal("=")) {
-  //     lexer.next();
-  //     value = lexer.getToken().token.trim();
-  //     lexer.next();
-  //   }
-  //   switch (key) {
-  //     case "SCORE":
-  //       {
-  //         try {
-  //           data.score = int.parse(value);
-  //         } catch (e) {
-  //           exercise.error += ' Score value must be integral. ';
-  //         }
-  //         break;
-  //       }
-  //     case "DIFF":
-  //       {
-  //         data.diffVariableId = value; // TODO: check, if string is valid
-  //         break;
-  //       }
-  //     default:
-  //       {
-  //         exercise.error += ' Unknown key $key. ';
-  //       }
-  //   }
-  // }
-  // check if variable exists
-  if (exerciseData.variables.contains(data.variableId) == false) {
-    exercise.error += ' Variable "${data.variableId}" is unknown'
-        ' (you may need to declare it with "let"). ';
     return inputField;
   }
   // optional: index (e.g. element of a vector)
@@ -171,18 +85,13 @@ MbclLevelItem parseInputElement(
       return inputField;
     }
   }
-  // ===== gap input =====
-  if (isGap) {
-    return inputField;
-  }
   // ===== term input =====
-  else if (exerciseData.functionVariables.contains(data.variableId)) {
+  if (exerciseData.functionVariables.contains(data.variableId)) {
     data.type = MbclInputFieldType.term;
     data.isFunction = true;
-    return inputField;
   }
   // ===== operand input =====
-  else {
+  else if (isGap == false) {
     // get type and subtype. The subtype refers to indexed types (e.g. the type
     // is "vector" and the subType "integer".)
     var opType = OperandType.values
@@ -223,6 +132,160 @@ MbclLevelItem parseInputElement(
       default:
         exercise.error += ' UNIMPLEMENTED input type ${opType.name}. ';
     }
-    return inputField;
   }
+  // parse optional attributes e.g. ",SCORE=", ",DIFF=x", ...
+  Map<String, String> attributes = {};
+  while (lexer.isTerminal(",")) {
+    lexer.next();
+    var key = lexer.getToken().token.trim();
+    lexer.next();
+    var value = "";
+    if (lexer.isTerminal("=")) {
+      lexer.next();
+      value = lexer.getToken().token.trim();
+      lexer.next();
+      while (lexer.isTerminal("+")) {
+        value += "+";
+        lexer.next();
+        value += lexer.getToken().token.trim(); // TODO: "+" could be within STR
+        lexer.next();
+      }
+    }
+    if (value.isEmpty) value = "true";
+    attributes[key] = value;
+  }
+  // process optional attributes
+  try {
+    // TODO: check semantics for each attribute (e.g. non-negative score, ...)!!
+    checkAttributes(attributes, [
+      "DIFF",
+      "SCORE",
+      "ARRANGE",
+      "ROWS",
+      "COLS",
+      "KEYBOARD",
+      "CHOICES",
+      "TOKENS",
+      "HIDE_LENGTH",
+      "SHOW_ALL_LETTERS",
+    ]);
+    // DIFF
+    data.diffVariableId = "";
+    if (hasAttribute(attributes, "DIFF")) {
+      data.diffVariableId = getAttributeIdentifier(attributes, "DIFF");
+    }
+    // SCORES
+    data.score = getAttributeInt(attributes, "SCORE", 1);
+    // ARRANGE
+    data.arrange = getAttributeBool(attributes, "ARRANGE", false);
+    // ROWS
+    data.dynamicRows = getAttributeString(
+            attributes, "ROWS", ["dynamic", "static"], "static") ==
+        "dynamic";
+    // COLS
+    data.dynamicCols = getAttributeString(
+            attributes, "COLS", ["dynamic", "static"], "static") ==
+        "dynamic";
+    // KEYBOARD
+    data.forceKeyboardId = getAttributeIdentifier(attributes, "KEYBOARD");
+    // CHOICES | TOKENS
+    if (attributes.containsKey("CHOICES") || attributes.containsKey("TOKENS")) {
+      var parsingChoices = attributes.containsKey("CHOICES");
+      var numChoices = 1;
+      var termTokensFactor = 1.0;
+      List<String> tk = [];
+      if (parsingChoices) {
+        // -- choices --
+        data.choices = true;
+        tk = attributes["CHOICES"]!.split("+");
+        try {
+          numChoices = int.parse(tk[0]); // TODO: check value
+        } catch (e) {
+          throw Exception(
+              "Attribute value of 'CHOICES' must be an integral number. ");
+        }
+      } else {
+        // -- term tokens --
+        data.termTokens = true;
+        tk = attributes["TOKENS"]!.split("+");
+        try {
+          termTokensFactor = double.parse(tk[0]); // TODO: check value
+        } catch (e) {
+          throw Exception(
+              "Attribute value of 'TOKENS' must be a floating point number. ");
+        }
+      }
+      for (var i = 0; i < exerciseData.instances.length; i++) {
+        var instance = exerciseData.instances[i];
+        var expected =
+            instance[(data.isFunction ? "@" : "") + data.variableId]!;
+        var expectedTerm = Parser().parse(expected);
+        if (data.index >= 0 && data.index < expectedTerm.o.length) {
+          expectedTerm = expectedTerm.o[data.index];
+        }
+        if (parsingChoices) {
+          // -- choices --
+          Set<String> choices = {};
+          // add expected solution
+          choices.add(expected.trim());
+          // check, if predefined terms are valid; and substitute variables
+          for (var choice in tk.sublist(1)) {
+            try {
+              // parse choice
+              var term = Parser().parse(choice);
+              // for all variables v: substitute the value of v (only applies
+              // if it present)
+              for (var id in instance.keys) {
+                if (choice.contains(id) == false ||
+                    id.endsWith(".tex") ||
+                    id.startsWith("@") ||
+                    id.startsWith("CHOICES") ||
+                    id.startsWith("TOKENS")) continue;
+                var replacementTerm = Parser().parse(instance[id]!);
+                // substitute only if replacement is an operand
+                if (replacementTerm.op == '#') {
+                  term = term.substituteVariableByTermOrOperand(
+                      id, Term.createConstInt(0), replacementTerm.value);
+                }
+              }
+              choices.add(term.toString());
+            } catch (e) {
+              throw Exception(
+                  "Manually added choice '$choice' has an invalid syntax. ");
+            }
+          }
+          for (var j = 0; j < numChoices - 1; j++) {
+            var choice = '';
+            const maxTries = 25;
+            for (var k = 0; k < maxTries; k++) {
+              var clone = expectedTerm.clone();
+              clone.disturb();
+              choice = clone.toString();
+              if (choices.contains(choice) == false) {
+                choices.add(choice);
+                break;
+              }
+            }
+          }
+          instance["CHOICES.${inputField.id}"] = choices.toList().join(" # ");
+        } else {
+          // -- term tokens --
+          Set<String> termTokens = tk.sublist(1).toSet();
+          var tmp = expectedTerm.tokenizeAndSynthesize(
+              removeDuplicates: true, depth: 2, synthFactor: termTokensFactor);
+          for (var t in tmp) {
+            termTokens.add(t.split("%%%")[0].trim()); // remove TeX part
+          }
+          instance["TOKENS.${inputField.id}"] = termTokens.toList().join(" # ");
+        }
+      }
+    }
+    // GAP
+    data.hideLengthOfGap = getAttributeBool(attributes, "HIDE_LENGTH", false);
+    data.showAllLettersOfGap =
+        getAttributeBool(attributes, "SHOW_ALL_LETTERS", false);
+  } catch (e) {
+    exercise.error += e.toString();
+  }
+  return inputField;
 }
