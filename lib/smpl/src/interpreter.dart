1db604cc00560376905881352a7891cb5fcb3185
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-03 14:57:01 +0100

    Update interpreter.dart

--- lib/smpl/src/interpreter.dart
+++ lib/smpl/src/interpreter.dart
@@ -16,6 +16,11 @@ class InterpreterSymbol {
   Term term = Term.createConst(Operand.createInt(0));
   Operand value = Operand.createInt(0);
   bool isFunction = false; // e.g. f(x) = x^2
+
+  String toString() {
+    var s = "$id;$term;$value;$isFunction";
+    return s;
+  }
 }
 
 class Interpreter {
@@ -75,140 +80,136 @@ class Interpreter {
   }
 
   void _assignment(Assignment assignment) {
-    InterpreterSymbol? symbol;
-    if (assignment.createSymbol) {
-      symbol = _addSymbol(assignment.lhs, assignment.row);
-    } else {
-      symbol = _getSymbol(assignment.lhs);
+    var symbol = _getSymbol(assignment.lhs);
+    symbol ??= _addSymbol(assignment.lhs, assignment.row);
+    var oldTerm = symbol.term;
+    var newTerm = _processTerm(assignment.rhs, assignment.vars, assignment.row);
+    symbol.term = newTerm;
+    if (assignment.isFunction) {
+      symbol.isFunction = true; // TODO: also OK for indexing??
+      // resolve diff, opt
+      symbol.term = Term.evalFunction(symbol.term);
+    } else if (assignment.rhs.replaceAll(" ", "").contains("opt(") ||
+        assignment.rhs.replaceAll(" ", "").contains("term(")) {
+      _error(
+          assignment.row,
+          '"opt(..)" and "term(..)" can ONLY be used, '
+          'if variable on left-hand side is a function (e.g. "f(x)")');
     }
-    if (symbol == null) {
-      _error(assignment.row, 'symbol "${assignment.lhs}" is unknown.');
-    } else {
-      var oldTerm = symbol.term;
-      var newTerm =
-          _processTerm(assignment.rhs, assignment.vars, assignment.row);
-      symbol.term = newTerm;
-      if (assignment.isFunction) {
-        symbol.isFunction = true; // TODO: also OK for indexing??
-        // resolve diff, opt
-        symbol.term = Term.evalFunction(symbol.term);
-      }
-      if (assignment.vars.isEmpty) {
-        var isOK = true;
-        var k = 0;
-        do {
-          if (assignment.lhsIndex1.isEmpty) {
-            symbol.value = symbol.term.eval({}); // TODO: error handling
-          } else {
-            var index1Term = _processTerm(
-                assignment.lhsIndex1, assignment.vars, assignment.row);
-            var index1Operand = index1Term.eval({});
-            var index1Value = index1Operand.real.toInt();
-            if (assignment.lhsIndex2.isNotEmpty) {
-              var index2Term = _processTerm(
-                  assignment.lhsIndex2, assignment.vars, assignment.row);
-              var index2Operand = index2Term.eval({});
-              var index2Value = index2Operand.real.toInt();
-              // 2-dim index
-              if (symbol.value.type != OperandType.matrix) {
-                _error(assignment.row, 'only matrices can be 2-dim indexed.');
-              }
-              if (index1Value < 0 || index1Value >= symbol.value.rows) {
-                _error(assignment.row, 'index value $index1Value is invalid.');
-              }
-              if (index2Value < 0 || index2Value >= symbol.value.cols) {
-                _error(assignment.row, 'index value $index2Value is invalid.');
-              }
-              symbol.value
-                      .items[index1Value * symbol.value.cols + index2Value] =
-                  symbol.term.eval({});
-              if (oldTerm.op == "matrix") {
-                symbol.term = oldTerm;
-                symbol.term.o[index1Value * symbol.value.cols + index2Value] =
-                    newTerm;
-              } else {
-                // hack (e.g. for "zeros")
-                symbol.term = Term.createConst(symbol.value);
-              }
+    if (assignment.vars.isEmpty) {
+      var isOK = true;
+      var k = 0;
+      do {
+        if (assignment.lhsIndex1.isEmpty) {
+          symbol.value = symbol.term.eval({}); // TODO: error handling
+        } else {
+          var index1Term = _processTerm(
+              assignment.lhsIndex1, assignment.vars, assignment.row);
+          var index1Operand = index1Term.eval({});
+          var index1Value = index1Operand.real.toInt();
+          if (assignment.lhsIndex2.isNotEmpty) {
+            var index2Term = _processTerm(
+                assignment.lhsIndex2, assignment.vars, assignment.row);
+            var index2Operand = index2Term.eval({});
+            var index2Value = index2Operand.real.toInt();
+            // 2-dim index
+            if (symbol.value.type != OperandType.matrix) {
+              _error(assignment.row, 'only matrices can be 2-dim indexed.');
+            }
+            if (index1Value < 0 || index1Value >= symbol.value.rows) {
+              _error(assignment.row, 'index value $index1Value is invalid.');
+            }
+            if (index2Value < 0 || index2Value >= symbol.value.cols) {
+              _error(assignment.row, 'index value $index2Value is invalid.');
+            }
+            symbol.value.items[index1Value * symbol.value.cols + index2Value] =
+                symbol.term.eval({});
+            if (oldTerm.op == "matrix") {
+              symbol.term = oldTerm;
+              symbol.term.o[index1Value * symbol.value.cols + index2Value] =
+                  newTerm;
             } else {
-              // 1-dim index
-              if (symbol.value.type != OperandType.vector) {
-                _error(assignment.row, 'only vectors can be indexed.');
-              }
-              if (index1Value < 0 || index1Value >= symbol.value.items.length) {
-                _error(assignment.row, 'index value $index1Value is invalid.');
-              }
-              symbol.value.items[index1Value] = symbol.term.eval({});
-              if (oldTerm.op == "vec") {
-                symbol.term = oldTerm;
-                symbol.term.o[index1Value] = newTerm;
-              } else {
-                // hack (e.g. for "zeros")
-                symbol.term = Term.createConst(symbol.value);
-              }
+              // hack (e.g. for "zeros")
+              symbol.term = Term.createConst(symbol.value);
+            }
+          } else {
+            // 1-dim index
+            if (symbol.value.type != OperandType.vector) {
+              _error(assignment.row, 'only vectors can be indexed.');
+            }
+            if (index1Value < 0 || index1Value >= symbol.value.items.length) {
+              _error(assignment.row, 'index value $index1Value is invalid.');
+            }
+            symbol.value.items[index1Value] = symbol.term.eval({});
+            if (oldTerm.op == "vec") {
+              symbol.term = oldTerm;
+              symbol.term.o[index1Value] = newTerm;
+            } else {
+              // hack (e.g. for "zeros")
+              symbol.term = Term.createConst(symbol.value);
             }
           }
-          isOK = true;
-          for (var i = 0; i < assignment.independentTo.length; i++) {
-            var indTo = assignment.independentTo[i];
-            var s2 = _getSymbol(indTo);
-            if (Operand.compareEqual(symbol.value, s2?.value as Operand)) {
-              isOK = false;
-              break;
-            }
+        }
+        isOK = true;
+        for (var i = 0; i < assignment.independentTo.length; i++) {
+          var indTo = assignment.independentTo[i];
+          var s2 = _getSymbol(indTo);
+          if (Operand.compareEqual(symbol.value, s2?.value as Operand)) {
+            isOK = false;
+            break;
           }
-          k++;
-          if (k >= 50) {
-            _error(
-              assignment.row,
-              'failed to create different variable values for "${symbol.id}".',
-            );
-          }
-        } while (isOK == false);
-        if (_verbose) {
-          print(
-            '>> assigned ${symbol.id}'
-            ' := ${symbol.value} (${symbol.value.type.name})',
+        }
+        k++;
+        if (k >= 50) {
+          _error(
+            assignment.row,
+            'failed to create different variable values for "${symbol.id}".',
           );
         }
-        if (assignment.expectedRhs.isNotEmpty) {
-          var expectedType = OperandType.int;
-          try {
-            expectedType = OperandType.values.byName(assignment.expectedType);
-          } catch (e) {
-            _error(assignment.row,
-                'unknown/unimplemented type "${assignment.expectedType}".');
-          }
-          if (symbol.value.type != expectedType) {
+      } while (isOK == false);
+      if (_verbose) {
+        print(
+          '>> assigned ${symbol.id}'
+          ' := ${symbol.value} (${symbol.value.type.name})',
+        );
+      }
+      if (assignment.expectedRhs.isNotEmpty) {
+        var expectedType = OperandType.int;
+        try {
+          expectedType = OperandType.values.byName(assignment.expectedType);
+        } catch (e) {
+          _error(assignment.row,
+              'unknown/unimplemented type "${assignment.expectedType}".');
+        }
+        if (symbol.value.type != expectedType) {
+          _error(
+              assignment.row,
+              'TEST FAILED: expected type "${expectedType.name}",'
+              ' but got type "${symbol.value.type.name}".');
+        }
+        Operand? expectedValue;
+        try {
+          var expectedTerm = _termParser.parse(assignment.expectedRhs,
+              splitIdentifiers: false);
+          expectedValue = expectedTerm.eval({});
+        } catch (e) {
+          _error(assignment.row,
+              'error in expected-term "$assignment.expectedRhs":$e.');
+        }
+        if (Operand.compareEqual(expectedValue!, symbol.value) == false) {
+          _error(
+              assignment.row,
+              'expected value "${expectedValue.toString()}",'
+              ' but got value "${symbol.value.toString()}".');
+        }
+        if (assignment.expectedStringifiedTerm.isNotEmpty) {
+          var expected = assignment.expectedStringifiedTerm;
+          var actual = symbol.term.toString();
+          if (expected.replaceAll(' ', '') != actual.replaceAll(' ', '')) {
             _error(
                 assignment.row,
-                'TEST FAILED: expected type "${expectedType.name}",'
-                ' but got type "${symbol.value.type.name}".');
-          }
-          Operand? expectedValue;
-          try {
-            var expectedTerm = _termParser.parse(assignment.expectedRhs,
-                splitIdentifiers: false);
-            expectedValue = expectedTerm.eval({});
-          } catch (e) {
-            _error(assignment.row,
-                'error in expected-term "$assignment.expectedRhs":$e.');
-          }
-          if (Operand.compareEqual(expectedValue!, symbol.value) == false) {
-            _error(
-                assignment.row,
-                'expected value "${expectedValue.toString()}",'
-                ' but got value "${symbol.value.toString()}".');
-          }
-          if (assignment.expectedStringifiedTerm.isNotEmpty) {
-            var expected = assignment.expectedStringifiedTerm;
-            var actual = symbol.term.toString();
-            if (expected.replaceAll(' ', '') != actual.replaceAll(' ', '')) {
-              _error(
-                  assignment.row,
-                  'expected stringified term "$expected",'
-                  ' but got "$actual"');
-            }
+                'expected stringified term "$expected",'
+                ' but got "$actual"');
           }
         }
       }
