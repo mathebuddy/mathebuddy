10de6787bf041bd559b2ca91b85b0eb4cea15a12
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-06 20:23:39 +0100

    Update update-testdata.sh

--- lib/compiler/test/update-testdata.sh
+++ lib/compiler/test/update-testdata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-rm -rf data/
-if [ -d "../../../../mathebuddy-public-courses" ]; then
-    cp -r ../../../../mathebuddy-public-courses data
-else
-    git clone https://github.com/mathebuddy/mathebuddy-public-courses.git data
-fi
-rm -rf data/.git*
-rm -rf data/.vscode
-rm data/LICENSE
-rm data/README.md
-rm data/server.py
+# rm -rf data/
+# if [ -d "../../../../mathebuddy-public-courses" ]; then
+#     cp -r ../../../../mathebuddy-public-courses data
+# else
+#     git clone https://github.com/mathebuddy/mathebuddy-public-courses.git data
+# fi
+# rm -rf data/.git*
+# rm -rf data/.vscode
+# rm data/LICENSE
+# rm data/README.md
+# rm data/server.py
