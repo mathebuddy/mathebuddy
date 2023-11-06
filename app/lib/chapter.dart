b4c574add0625db17b94f95639f5d6e8cbac30d9
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-06 20:24:53 +0100

    Update chapter.dart

--- app/lib/chapter.dart
+++ app/lib/chapter.dart
@@ -8,12 +8,14 @@
 /// This file implements the chapter widget that contains the list of units.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:mathebuddy/mbcl/src/chapter.dart';
 
-import 'appbar.dart';
-import 'unit.dart';
-import 'error.dart';
+import 'package:mathebuddy/appbar.dart';
+import 'package:mathebuddy/style.dart';
+import 'package:mathebuddy/unit.dart';
+import 'package:mathebuddy/error.dart';
 
 class ChapterWidget extends StatefulWidget {
   final MbclChapter chapter;
@@ -35,44 +37,96 @@ class ChapterState extends State<ChapterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    List<TableRow> tableRows = [];
-    for (var unit in widget.chapter.units) {
-      tableRows.add(TableRow(children: [
-        TableCell(
-          child: GestureDetector(
-              onTap: () {
-                var route = MaterialPageRoute(builder: (context) {
-                  return UnitWidget(widget.chapter, unit);
-                });
-                Navigator.push(context, route).then((value) => setState(() {}));
-              },
-              child: Container(
-                  margin: EdgeInsets.all(2.0),
-                  height: 40.0,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          width: 2.0, color: Color.fromARGB(255, 81, 81, 81)),
-                      borderRadius: BorderRadius.circular(100.0)),
-                  child: Center(child: Text(unit.title)))),
-        )
-      ]));
-    }
+    // logo
+    Widget logo = ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 50),
+        child: Image.asset('assets/img/logo.png'));
 
+    // title
+    Widget title = Padding(
+        padding: EdgeInsets.only(top: 20.0, left: 10, right: 10, bottom: 20),
+        child: Center(
+            child: Text(widget.chapter.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: getStyle().courseTitleFontColor,
+                    fontSize: getStyle().courseTitleFontSize,
+                    fontWeight: getStyle().courseTitleFontWeight))));
+
+    // units
+    List<TableRow> tableRows = [];
+    List<TableCell> tableCells = [];
+    var cellColor = Colors.white;
+    for (var i = 0; i < widget.chapter.units.length; i++) {
+      var unit = widget.chapter.units[i];
+      Widget icon = Text("");
+      if (unit.iconData.isNotEmpty) {
+        icon = SvgPicture.string(unit.iconData, color: cellColor);
+      }
+      icon = SizedBox(height: 75, child: icon);
+      Widget content = Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(children: [
+            icon,
+            Wrap(children: [
+              Text(unit.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: cellColor,
+                      fontWeight: FontWeight.w400))
+            ])
+          ]));
+      tableCells.add(TableCell(
+        //verticalAlignment: TableCellVerticalAlignment.fill,
+        child: GestureDetector(
+            onTap: () {
+              var route = MaterialPageRoute(builder: (context) {
+                return UnitWidget(widget.chapter, unit);
+              });
+              Navigator.push(context, route).then((value) => setState(() {}));
+            },
+            child: Container(
+                margin: EdgeInsets.all(2.0),
+                decoration: BoxDecoration(
+                    color: getStyle()
+                        .matheBuddyRed //courseColors[i % getStyle().courseColors.length]
+                    //.withOpacity(0.95)
+                    ,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Center(child: content))),
+      ));
+    }
+    if ((tableCells.length % 2) != 0) {
+      tableCells.add(TableCell(child: Text("")));
+    }
+    var numRows = (tableCells.length / 2).ceil();
+    for (var i = 0; i < numRows; i++) {
+      List<TableCell> columns = [];
+      for (var j = 0; j < 2; j++) {
+        columns.add(tableCells[i * 2 + j]);
+      }
+      tableRows.add(TableRow(children: columns));
+    }
     var unitsTable = Table(
       defaultVerticalAlignment: TableCellVerticalAlignment.middle,
       children: tableRows,
     );
 
-    Widget contents = Column(children: [unitsTable]);
+    List<Widget> contents = [
+      //logo,
+      title, unitsTable
+    ];
 
     if (widget.chapter.error.isNotEmpty) {
-      contents = generateErrorWidget(widget.chapter.error);
+      var err = generateErrorWidget(widget.chapter.error);
+      contents.insert(0, err);
     }
 
-    var body =
-        SingleChildScrollView(padding: EdgeInsets.all(5), child: contents);
+    var body = SingleChildScrollView(
+        padding: EdgeInsets.all(5), child: Column(children: contents));
     return Scaffold(
-      appBar: buildAppBar(this, false, null),
+      appBar: buildAppBar(this, null),
       body: body,
       backgroundColor: Colors.white,
     );
