e1fd16eb7739950d0dcea181c760d3b084d33647
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-06 20:25:03 +0100

    Update error.dart

--- app/lib/error.dart
+++ app/lib/error.dart
@@ -1,3 +1,10 @@
+/// mathe:buddy - a gamified app for higher math
+/// https://mathebuddy.github.io/
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
 import 'package:flutter/material.dart';
 
 Widget generateErrorWidget(String errorText) {
