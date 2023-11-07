47b7254171a9449b4e486c00afd4d961a77e39a6
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-07 14:13:26 +0100

    Create chat.dart

---
+++ lib/compiler/src/chat.dart
@@ -0,0 +1,58 @@
+/// mathe:buddy - a gamified app for higher math
+/// https://mathebuddy.github.io/
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+import '../../mbcl/src/course.dart';
+import '../../mbcl/src/level.dart';
+import '../../mbcl/src/level_item.dart';
+import '../../mbcl/src/chat.dart';
+
+class ChatInformationRetrieval {
+  MbclChat run(MbclCourse course) {
+    var chatData = MbclChat();
+    for (var chapter in course.chapters) {
+      for (var level in chapter.levels) {
+        for (var item in level.items) {
+          /// TODO: include other def* types
+          if (item.type == MbclLevelItemType.defDefinition) {
+            if (item.chatKeys.isNotEmpty) {
+              var chatKeys = item.chatKeys.split(",");
+              for (var chatKey in chatKeys) {
+                // TODO (low prio): high memory consumption in case of many keys
+                // TODO (low prio): better save data only once
+                var levelPath = "${chapter.fileId}/${level.fileId}";
+                var pseudoLevel = MbclLevel();
+                var paragraph = extractParagraph(item, pseudoLevel);
+                var cd = MbclChatDefinition(chatKey, levelPath, paragraph);
+                chatData.definitions[chatKey] = cd;
+              }
+            }
+          }
+        }
+      }
+    }
+    var bp = 1337;
+    return chatData;
+  }
+
+  MbclLevelItem extractParagraph(MbclLevelItem item, MbclLevel level) {
+    MbclLevelItem p = MbclLevelItem(level, MbclLevelItemType.paragraph, -1);
+    for (var subItem in item.items) {
+      switch (subItem.type) {
+        case MbclLevelItemType.paragraph:
+          p.items.addAll(subItem.items);
+          break;
+        case MbclLevelItemType.equation:
+          var data = subItem.equationData!;
+          p.items.add(data.math!);
+          break;
+        default:
+        // TODO
+      }
+    }
+    return p;
+  }
+}
