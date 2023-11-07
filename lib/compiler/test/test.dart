47107f8f3f18f374e0475e00a36771f923e7cca0
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-07 20:24:01 +0100

    Update test.dart

--- lib/compiler/test/test.dart
+++ lib/compiler/test/test.dart
@@ -81,8 +81,9 @@ void main() {
   var files = [
     'smoke-test.mbl',
     //'../../../../mathebuddy-public-courses/demo-course/course.mbl'
-    //'../../../../mathebuddy-public-courses/demo-course/course.mbl'
     //'../../../../mathebuddy-public-courses/demo-course/basics/index.mbl'
+    //'../../../../mathebuddy-private-courses/hm1/komplex/index.mbl',
+    //'../../../../mathebuddy-private-courses/hm1/course.mbl',
     //'../../../../mathebuddy-public-courses/demo-basic/exercises.mbl',
     //'../../../../mathebuddy-public-courses/demo-basic/hello.mbl',
     //'../../../../mathebuddy-public-courses/demo-basic/typography.mbl',
