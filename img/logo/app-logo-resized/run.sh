cf68f6c1ae3a6053957d0fd254a33cee604f2f0b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-11-09 15:37:10 +0100

    added ios and macOS Icons

---
+++ img/logo/app-logo-resized/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# image magick needs to be insalled, e.g. on macOS $brew install imagemagick
+convert logo1024.png -resize 16 logo16.png
+convert logo1024.png -resize 20 logo20.png
+convert logo1024.png -resize 29 logo29.png
+convert logo1024.png -resize 32 logo32.png
+convert logo1024.png -resize 64 logo64.png
+convert logo1024.png -resize 40 logo40.png
+convert logo1024.png -resize 58 logo58.png
+convert logo1024.png -resize 60 logo60.png
+convert logo1024.png -resize 76 logo76.png
+convert logo1024.png -resize 80 logo80.png
+convert logo1024.png -resize 87 logo87.png
+convert logo1024.png -resize 120 logo120.png
+convert logo1024.png -resize 128 logo128.png
+convert logo1024.png -resize 152 logo152.png
+convert logo1024.png -resize 167 logo167.png
+convert logo1024.png -resize 180 logo180.png
+convert logo1024.png -resize 256 logo256.png
+convert logo1024.png -resize 512 logo512.png
