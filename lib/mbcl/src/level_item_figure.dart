d29449f0ee7b51e625440454f89012b4acf67cdc
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 14:25:15 +0100

    Create level_item_figure.dart

---
+++ lib/mbcl/src/level_item_figure.dart
@@ -0,0 +1,46 @@
+/// mathe:buddy - a gamified app for higher math
+/// https://mathebuddy.github.io/
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+// refer to the specification at https://mathebuddy.github.io/mathebuddy/
+
+import 'level_item.dart';
+
+class MbclFigureData {
+  MbclLevelItem figure;
+  String filePath = '';
+  String code = '';
+  String data = '';
+  int widthPercentage = 100;
+  List<MbclLevelItem> caption = [];
+
+  bool zoomed = false;
+
+  MbclFigureData(this.figure);
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "filePath": filePath,
+      "code": code,
+      "data": data,
+      "widthPercentage": widthPercentage,
+      "caption": caption.map((e) => e.toJSON()).toList(),
+    };
+  }
+
+  fromJSON(Map<String, dynamic> src) {
+    filePath = src["filePath"];
+    code = src["code"];
+    data = src["data"];
+    widthPercentage = src["widthPercentage"];
+    int n = src["caption"].length;
+    for (var i = 0; i < n; i++) {
+      var cap = MbclLevelItem(figure.level, MbclLevelItemType.error, -1);
+      cap.fromJSON(src["caption"][i]);
+      caption.add(cap);
+    }
+  }
+}
