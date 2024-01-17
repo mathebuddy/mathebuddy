2820b1ccf56e0486aa997c25e6cfe88649ae66f8
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:49:28 +0100

    Update level_table.dart

--- app/lib/level_table.dart
+++ app/lib/level_table.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -12,9 +12,9 @@ import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 import 'package:mathebuddy/mbcl/src/level_item_table.dart';
 import 'package:mathebuddy/mbcl/src/level.dart';
 
-import 'package:mathebuddy/level.dart';
+import 'package:mathebuddy/widget_level.dart';
 
-Widget generateTable(LevelState state, MbclLevel level, MbclLevelItem item,
+Widget generateTable(State state, MbclLevel level, MbclLevelItem item,
     {MbclExerciseData? exerciseData}) {
   var tableData = item.tableData as MbclTableData;
   List<TableRow> rows = [];
