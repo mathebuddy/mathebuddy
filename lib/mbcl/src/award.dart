9e8fafc2ede24a2fd88f890089242ed7b48dd825
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:54:05 +0100

    Create award.dart

---
+++ lib/mbcl/src/award.dart
@@ -0,0 +1,16 @@
+/// mathe:buddy - a gamified app for higher math
+/// https://mathebuddy.github.io/
+/// (c) 2022-2024 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+// refer to the specification at https://mathebuddy.github.io/mathebuddy/
+
+class MbclAward {
+  final String id;
+  final String text;
+  final DateTime? dateTime; // null, if award was NOT earned
+
+  MbclAward(this.id, this.text, this.dateTime);
+}
