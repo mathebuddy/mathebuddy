3adc97af392b97146305a55597e56d18d9d8e606
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 14:23:25 +0100

    Update level_exercise.dart

--- app/lib/level_exercise.dart
+++ app/lib/level_exercise.dart
@@ -8,9 +8,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:mathebuddy/mbcl/src/level_item.dart';
+import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 import 'package:mathebuddy/mbcl/src/level.dart';
-
-import 'package:mathebuddy/math-runtime/src/parse.dart' as term_parser;
 
 import 'package:mathebuddy/main.dart';
 import 'package:mathebuddy/screen.dart';
@@ -21,70 +20,15 @@ void evaluateExercise(
     LevelState state, MbclLevel level, MbclExerciseData exerciseData) {
   print("----- evaluating exercise -----");
   state.keyboardState.layout = null;
-  // check exercise: TODO must implement in e.g. new file exercise.dart
-  var allCorrect = true;
-  for (var inputFieldId in exerciseData.inputFields.keys) {
-    var inputField =
-        exerciseData.inputFields[inputFieldId] as MbclInputFieldData;
-    var ok = false;
-    try {
-      var studentTerm = term_parser.Parser().parse(inputField.studentValue);
-      if (inputField.diffVariableId.isNotEmpty) {
-        var studentTermDiff =
-            studentTerm.diff(inputField.diffVariableId).optimize();
-        print("diff student answer: $studentTerm -> $studentTermDiff");
-        studentTerm = studentTermDiff;
-      }
-      var expected = inputField.expectedValue;
-      if (inputField.type == MbclInputFieldType.string) {
-        expected = expected.toUpperCase();
-      }
-      var expectedTerm = term_parser.Parser().parse(expected);
-      print("comparing $studentTerm to $expectedTerm");
-      ok = expectedTerm.compareNumerically(studentTerm);
-    } catch (e) {
-      // TODO: give GUI feedback, that term is not well formed, ...
-      print("evaluating answer failed: $e");
-      ok = false;
-    }
-    if (ok) {
-      print("answer OK");
-    } else {
-      allCorrect = false;
-      print("answer wrong: expected ${inputField.expectedValue},"
-          " got ${inputField.studentValue}");
-    }
-  }
-  if (allCorrect) {
-    print("... all answers are correct!");
-    exerciseData.feedback = MbclExerciseFeedback.correct;
-  } else {
-    print("... at least one answer is incorrect!");
-    exerciseData.feedback = MbclExerciseFeedback.incorrect;
-  }
+  exerciseData.evaluate();
   level.calcProgress();
-  print("----- end of exercise evaluation -----");
+  level.chapter.saveUserData();
 }
 
 Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
-  // TODO: must report error, if "exerciseData.instances.length" == 0!!
+  // TODO: must report error, if "exerciseData.numInstances" == 0!!
   exerciseKey = GlobalKey();
-  var exerciseData = item.exerciseData as MbclExerciseData;
-  if (exerciseData.runInstanceIdx < 0) {
-    // create order, if not yet done
-    if (exerciseData.randomInstanceOrder.isEmpty) {
-      exerciseData.randomInstanceOrder = [
-        for (var i = 0; i < exerciseData.instances.length; i++) i
-      ];
-      exerciseData.randomInstanceOrder.shuffle();
-    }
-    // proceed to next instance
-    exerciseData.runInstanceIdx =
-        exerciseData.randomInstanceOrder[exerciseData.randomInstanceOrderIdx];
-    exerciseData.randomInstanceOrderIdx =
-        (exerciseData.randomInstanceOrderIdx + 1) %
-            exerciseData.instances.length;
-  }
+  var exerciseData = item.exerciseData!;
   List<Widget> list = [];
   if (debugMode && exerciseData.requiredExercises.isNotEmpty) {
     var text = 'DEBUG INFO: This exercise depends on [';
@@ -95,11 +39,10 @@ Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
       }
     }
     text += ']';
-    list.add(Container(
-        child: Text(
+    list.add(Text(
       text,
       style: TextStyle(color: Colors.grey),
-    )));
+    ));
   }
   if (level.disableBlockTitles) {
     list.add(Text(
@@ -126,7 +69,7 @@ Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
   }
   if (debugMode) {
     // show random instances, scores, time
-    var text = "\u2684 ${exerciseData.instances.length}"
+    var text = "\u2684 ${exerciseData.numInstances}"
         "   \u2211 ${exerciseData.scores}";
     if (exerciseData.time >= 0) {
       text += "   \u231b${exerciseData.time}";
@@ -245,7 +188,7 @@ Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
     if (debugMode ||
         (exerciseData.disableRetry == false &&
             exerciseData.feedback == MbclExerciseFeedback.correct &&
-            exerciseData.instances.length > 1)) {
+            exerciseData.numInstances > 1)) {
       buttons.add(retryButton);
     }
   }
@@ -257,31 +200,9 @@ Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
           opacity = item == state.activeExercise ? 1.0 : 0.3;
         }*/
 
-  if (debugMode) {
+  if (debugMode && exerciseData.instances.isNotEmpty) {
     // show variable values
-    var instance = exerciseData.instances[exerciseData.runInstanceIdx];
-    var text = "";
-    for (var key in instance.keys) {
-      if (key.startsWith("__") || key.endsWith(".tex") || key.startsWith("@")) {
-        continue;
-      }
-      if (text.isNotEmpty) text += "\n";
-      var value = instance[key];
-      text += "$key=$value";
-
-      var term = "";
-      var opt = "";
-      if (instance.containsKey("@$key")) {
-        term = instance["@$key"]!;
-      }
-      if (instance.containsKey("@@$key")) {
-        opt = instance["@@$key"]!;
-      }
-      if (term.contains("rand") == false) {
-        text += ", term($key)=$term";
-        text += ", opt($key)=$opt";
-      }
-    }
+    var text = exerciseData.getVariableValuesAsString();
     if (text.isNotEmpty) {
       list.add(Row(mainAxisAlignment: MainAxisAlignment.start, children: [
         Flexible(
@@ -298,33 +219,16 @@ Widget generateExercise(LevelState state, MbclLevel level, MbclLevelItem item) {
                             child: Text(" $text ",
                                 style: TextStyle(color: Colors.white)))))))
       ]));
-      //list.add(Row(children: [Flexible(child: Text(text))]));
     }
   }
 
   return Opacity(
       opacity: opacity,
       child: Container(
-          // decoration: BoxDecoration(color: Colors.white, boxShadow: [
-          //   BoxShadow(
-          //       color: Colors.grey.withOpacity(0.08),
-          //       spreadRadius: 5,
-          //       blurRadius: 7,
-          //       offset: Offset(0, 3))
-          // ]),
           decoration: BoxDecoration(
-              //color: const Color.fromARGB(15, 200, 200, 200),
               color: feedbackColor.withOpacity(0.03),
               border: Border.all(color: feedbackColor, width: 1.0),
-              borderRadius: BorderRadius.circular(5)
-              // border: Border(
-              //     top: BorderSide(
-              //         color: const Color.fromARGB(255, 233, 233, 233),
-              //         width: 1.5),
-              //     bottom: BorderSide(
-              //         color: const Color.fromARGB(255, 233, 233, 233),
-              //         width: 1.5))
-              ),
+              borderRadius: BorderRadius.circular(5)),
           padding: EdgeInsets.only(bottom: 10.0),
           margin: EdgeInsets.only(top: 20.0, bottom: 10.0),
           child: Column(
