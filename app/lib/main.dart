f565cb2657248a518c85867023233c8bb9ed54c3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:49:34 +0100

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 import 'package:mathebuddy/mbcl/src/course.dart';
 
 import 'package:mathebuddy/color.dart';
-import 'package:mathebuddy/home.dart';
+import 'package:mathebuddy/widget_home.dart';
 
 var showDebugReleaseSwitch = true;
 var debugMode = true;
