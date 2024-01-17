4a94c7d1c83e3c0e497128dd32f675ff678ba05e
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:49:32 +0100

    Update level_todo.dart

--- app/lib/level_todo.dart
+++ app/lib/level_todo.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -11,9 +11,7 @@ import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 import 'package:mathebuddy/mbcl/src/level.dart';
 
-import 'package:mathebuddy/level.dart';
-
-Widget generateTodo(LevelState state, MbclLevel level, MbclLevelItem item,
+Widget generateTodo(State state, MbclLevel level, MbclLevelItem item,
     {MbclExerciseData? exerciseData}) {
   var opacity = 1.0;
   List<Widget> list = [];
