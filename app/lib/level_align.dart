aca112259d4e5a6ecedf731a914c88388e23dfe3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:48:56 +0100

    Update level_align.dart

--- app/lib/level_align.dart
+++ app/lib/level_align.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -11,9 +11,9 @@ import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 import 'package:mathebuddy/mbcl/src/level.dart';
 
-import 'package:mathebuddy/level.dart';
+import 'package:mathebuddy/widget_level.dart';
 
-Widget generateAlign(LevelState state, MbclLevel level, MbclLevelItem item,
+Widget generateAlign(State state, MbclLevel level, MbclLevelItem item,
     {MbclExerciseData? exerciseData}) {
   List<Widget> list = [];
   for (var subItem in item.items) {
