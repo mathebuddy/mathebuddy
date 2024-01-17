48c0eebaae5bd77bbc5561fd8af46082e8838d9e
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:49:23 +0100

    Update level_paragraph_item_math.dart

--- app/lib/level_paragraph_item_math.dart
+++ app/lib/level_paragraph_item_math.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -13,9 +13,8 @@ import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 
 import 'package:mathebuddy/screen.dart';
-import 'package:mathebuddy/level.dart';
 
-InlineSpan generateParagraphItemMath(LevelState state, MbclLevelItem item,
+InlineSpan generateParagraphItemMath(State state, MbclLevelItem item,
     {bold = false,
     italic = false,
     color = Colors.black,
