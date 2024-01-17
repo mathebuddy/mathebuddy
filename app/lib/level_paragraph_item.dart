84c756cd7581d2765479d29d2337b01f36c2a4d1
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 21:49:18 +0100

    Update level_paragraph_item.dart

--- app/lib/level_paragraph_item.dart
+++ app/lib/level_paragraph_item.dart
@@ -1,6 +1,6 @@
 /// mathe:buddy - a gamified app for higher math
 /// https://mathebuddy.github.io/
-/// (c) 2022-2023 by TH Koeln
+/// (c) 2022-2024 by TH Koeln
 /// Author: Andreas Schwenk contact@compiler-construction.com
 /// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
 /// License: GPL-3.0-or-later
@@ -10,13 +10,12 @@ import 'package:flutter/material.dart';
 import 'package:mathebuddy/mbcl/src/level_item.dart';
 import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 
-import 'package:mathebuddy/level.dart';
 import 'package:mathebuddy/screen.dart';
 import 'package:mathebuddy/level_paragraph_item_math.dart';
 import 'package:mathebuddy/level_paragraph_item_input_field.dart';
 import 'package:mathebuddy/style.dart';
 
-InlineSpan generateParagraphItem(LevelState state, MbclLevelItem item,
+InlineSpan generateParagraphItem(State state, MbclLevelItem item,
     {bold = false,
     italic = false,
     color = Colors.black,
@@ -123,7 +122,7 @@ InlineSpan generateParagraphItem(LevelState state, MbclLevelItem item,
             italic: italic,
             color: color,
             exerciseData: exerciseData);
-        state.activeInputFields.add(f);
+        //state.activeInputFields.add(f);
         return inputField;
       }
     default:
