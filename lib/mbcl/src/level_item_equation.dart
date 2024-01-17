952ecc3d89fe5ff0f230f4732887585a1291cdf1
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-17 14:25:11 +0100

    Create level_item_equation.dart

---
+++ lib/mbcl/src/level_item_equation.dart
@@ -0,0 +1,34 @@
+/// mathe:buddy - a gamified app for higher math
+/// https://mathebuddy.github.io/
+/// (c) 2022-2023 by TH Koeln
+/// Author: Andreas Schwenk contact@compiler-construction.com
+/// Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+/// License: GPL-3.0-or-later
+
+// refer to the specification at https://mathebuddy.github.io/mathebuddy/
+
+import 'level_item.dart';
+
+class MbclEquationData {
+  MbclLevelItem equation;
+  MbclLevelItem? math;
+  int number = 0;
+  bool leftAligned = false;
+
+  MbclEquationData(this.equation);
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "math": math?.toJSON(),
+      "number": number,
+      "leftAligned": leftAligned,
+    };
+  }
+
+  fromJSON(Map<String, dynamic> src) {
+    math = MbclLevelItem(equation.level, MbclLevelItemType.error, -1);
+    math?.fromJSON(src["math"]);
+    number = src["number"];
+    leftAligned = src["leftAligned"] as bool;
+  }
+}
