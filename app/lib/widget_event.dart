9510e657a18bc58a5146cef8a0be423123e78bb9
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-01-23 13:45:18 +0100

    Update widget_event.dart

--- app/lib/widget_event.dart
+++ app/lib/widget_event.dart
@@ -7,22 +7,18 @@
 
 /// This file implements the level widget.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:mathebuddy/event.dart';
 import 'package:mathebuddy/widget_event_painter.dart';
 import 'package:mathebuddy/level_exercise.dart';
-import 'package:mathebuddy/level_item.dart';
 import 'package:mathebuddy/mbcl/src/chapter.dart';
 import 'package:mathebuddy/mbcl/src/course.dart';
 
 import 'package:mathebuddy/mbcl/src/level.dart';
 import 'package:mathebuddy/mbcl/src/level_item_exercise.dart';
 
-import 'package:mathebuddy/keyboard.dart';
 import 'package:mathebuddy/appbar.dart';
 import 'package:mathebuddy/main.dart';
 import 'package:mathebuddy/mbcl/src/unit.dart';
@@ -76,10 +72,6 @@ class EventState extends State<EventWidget> {
     var level = widget.level;
     level.calcProgress();
 
-    if (widget.eventData.eventState == EventDataState.init) {
-      widget.eventData.start(this);
-    }
-
     // debug: show level path
     if (debugMode && level.fileId.isNotEmpty) {
       var path = "${widget.chapter.fileId}/${level.fileId}.mbl";
@@ -128,15 +120,22 @@ class EventState extends State<EventWidget> {
                 painter: EventPainter(8, false, timePercentage)))));
 
     // jokers
-    List<String> jokerTexts = ["50:50", "time+", "blub"];
+    List<Widget> jokerTexts = [
+      Text("50:50", style: TextStyle(color: Colors.white, fontSize: 24)),
+      Icon(MdiIcons.fromString("clock-outline"), size: 36, color: Colors.white),
+      Text("", style: TextStyle(color: Colors.white)),
+    ];
+
     List<Widget> buttons = [];
     var buttonWidth = screenWidth / 3 - 5;
     var buttonHeight = 45.0;
-    for (var i = 0; i < 3; i++) {
-      var buttonText =
-          Text(jokerTexts[i], style: TextStyle(color: Colors.white));
+    for (var i = 0; i < jokerTexts.length; i++) {
+      var buttonText = jokerTexts[i];
       var jokerAvailable = i == 0 && widget.eventData.jokerAvailable5050 ||
           i == 1 && widget.eventData.jokerAvailableTimePlus;
+      if (widget.eventData.eventState == EventDataState.init) {
+        jokerAvailable = false;
+      }
       buttons.add(Positioned(
           left: i * buttonWidth + 5,
           top: 5,
@@ -175,7 +174,64 @@ class EventState extends State<EventWidget> {
             width: screenWidth, height: 50, child: Stack(children: buttons))));
 
     // exercise or game over
-    if (widget.eventData.eventState == EventDataState.gameOver) {
+    if (widget.eventData.eventState == EventDataState.init) {
+      var infoDE = [
+        "Dies ist ein Event-Level. Es kommt beim Lösen der Aufgaben auf die Zeit an.",
+        "Der obere Balken zeigt den Highscore. Löst Du eine Aufgabe richtig, so erhöht sich dieser Balken. Andernfalls verringert er sich. High Risk / High Impact: Je schneller Du die Lösung auswählst, umso größer ist die Auswirkung auf Deinen Highscore.",
+        "Der zweite Balken zeigt die verbleibende Zeit an.",
+        "Dir stehen einige Joker zur Verfügung. Diese kannst Du per Button anwenden."
+      ];
+      var infoEN = [
+        "This is an event level. Time is of the essence when solving the tasks.",
+        "The upper bar shows the high score. If you solve a task correctly, this bar increases. Otherwise it decreases. High Risk / High Impact: The faster you select the solution, the greater the impact on your high score.",
+        "The second bar shows the remaining time.",
+        "You have several jokers at your disposal. You can use these with the button."
+      ];
+      var info = language == "de" ? infoDE : infoEN;
+
+      for (var text in info) {
+        page.add(Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: RichText(
+                text: TextSpan(
+                    text: text,
+                    style: TextStyle(fontSize: 18, color: Colors.white)))));
+      }
+      var w = 100;
+      var h = 50;
+      var startButton = Positioned(
+          left: screenWidth / 2 - w / 2,
+          top: 20,
+          child: SizedBox(
+              width: w - 3,
+              height: h - 3,
+              child: GestureDetector(
+                onTap: () {
+                  widget.eventData.start(this);
+                  setState(() {});
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Style().matheBuddyGreen.withOpacity(0.9),
+                            Style().matheBuddyGreen.withOpacity(0.95)
+                          ]),
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text("Start",
+                        style: TextStyle(color: Colors.white, fontSize: 24))),
+              )));
+      page.add(Container(
+          //color: Colors.pink,
+          child: SizedBox(
+              width: screenWidth,
+              height: 75,
+              child: Stack(children: [startButton]))));
+    } else if (widget.eventData.eventState == EventDataState.gameOver) {
       page.add(Center(
           child: Text("GAME\nOVER",
               textAlign: TextAlign.center,
@@ -192,7 +248,9 @@ class EventState extends State<EventWidget> {
       }
       if (exercise != null) {
         page.add(generateExercise(this, level, exercise,
-            borderWidth: 0, generateInputFields: false));
+            borderWidth: 0,
+            generateInputFields: false,
+            eventColorScheme: true));
 
         var exerciseData = exercise.exerciseData!;
         var choices = exerciseData.getChoicesOfFirstInputField();
@@ -202,7 +260,7 @@ class EventState extends State<EventWidget> {
           var inputFieldData = inputField.inputFieldData!;
 
           var buttonWidth = screenWidth / 2 - 5;
-          var buttonHeight = 80.0;
+          var buttonHeight = 65.0;
 
           List<Widget> buttons = [];
           for (var i = 0; i < 4; i++) {
@@ -257,20 +315,20 @@ class EventState extends State<EventWidget> {
                           child: Container(
                               decoration: BoxDecoration(
                                 color: Colors.white,
-                                border: Border.all(
-                                    color:
-                                        const Color.fromARGB(255, 75, 75, 75),
-                                    width: 2),
+                                // border: Border.all(
+                                //     color:
+                                //         const Color.fromARGB(255, 75, 75, 75),
+                                //     width: 2),
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(15)),
-                                boxShadow: [
-                                  BoxShadow(
-                                      blurRadius: 3.0,
-                                      spreadRadius: 2.0,
-                                      offset: Offset(1, 1),
-                                      color: const Color.fromARGB(
-                                          255, 200, 200, 200))
-                                ],
+                                // boxShadow: [
+                                //   BoxShadow(
+                                //       blurRadius: 3.0,
+                                //       spreadRadius: 2.0,
+                                //       offset: Offset(1, 1),
+                                //       color: const Color.fromARGB(
+                                //           255, 200, 200, 200))
+                                // ],
                               ),
                               alignment: Alignment.center,
                               child: buttonText),
@@ -319,9 +377,9 @@ class EventState extends State<EventWidget> {
         generateLevelBottomNavigationBar(this, level, levelTitleKey!);
 
     return Scaffold(
-      appBar: buildAppBar(true, this, widget.chapter),
+      appBar: buildAppBar(true, false, this, context, widget.course),
       body: body,
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       bottomSheet: bottomArea,
     );
   }
