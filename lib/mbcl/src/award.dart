2b02ef5f0070f67ddefd74d90249b610e17a5549
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-03-18 16:33:36 +0100

    Update award.dart

--- lib/mbcl/src/award.dart
+++ lib/mbcl/src/award.dart
@@ -7,11 +7,80 @@
 
 // refer to the specification at https://mathebuddy.github.io/mathebuddy/
 
-class MbclAward {
-  final String id;
-  final String textEn;
-  final String textDe;
-  DateTime? dateTime; // null, if award was NOT earned
+enum MbclAwardType {
+  passedFirstLevel,
+  passedFirstUnit,
+  passedFirstChapter,
+  passedFirstGame,
+  played3daysInRow,
+  played5daysInRow,
+  played10daysInRow,
+}
 
-  MbclAward(this.id, this.textEn, this.textDe, this.dateTime);
+class MbclAwards {
+  // <AwardId, UnixTime>  time is 0 in case the award is not yet received
+  Map<String, int> awards = {};
+
+  MbclAwards() {
+    for (var type in MbclAwardType.values) {
+      awards[type.name] = 0;
+    }
+  }
+
+  Map<String, dynamic> toJSON() {
+    return awards;
+  }
+
+  /// returns true, if the award is new
+  bool enableAwardConditionally(MbclAwardType type) {
+    var alreadyAchieved = awards[type.name]! > 0;
+    if (alreadyAchieved) return false;
+    awards[type.name] = DateTime.now().millisecondsSinceEpoch;
+    return true;
+  }
+
+  fromJSON(Map<String, dynamic> src) {
+    for (var entry in src.entries) {
+      var awardId = entry.key;
+      var unixTimeReceived = entry.value;
+      if (awards.containsKey(awardId)) {
+        awards[awardId] = unixTimeReceived;
+      }
+    }
+  }
+
+  getText(String id, String language) {
+    switch (id) {
+      case "passedFirstLevel":
+        return language == "en"
+            ? "Passed the first level"
+            : "Erstes Level geschafft";
+      case "passedFirstUnit":
+        return language == "en"
+            ? "Passed the first unit"
+            : "Erste Unit geschafft";
+      case "passedFirstChapter":
+        return language == "en"
+            ? "Passed the first chapter"
+            : "Erstes Kapitel geschafft";
+      case "passedFirstGame":
+        return language == "en"
+            ? "Played the first game"
+            : "Erstes Spiel gespielt";
+      case "played3daysInRow":
+        return language == "en"
+            ? "Trained 3 days in a row"
+            : "3 Tage hintereinander gespielt";
+      case "played5daysInRow":
+        return language == "en"
+            ? "Trained 5 days in a row"
+            : "5 Tage hintereinander gespielt";
+      case "played10daysInRow":
+        return language == "en"
+            ? "Trained 10 days in a row"
+            : "10 Tage hintereinander gespielt";
+      default:
+        return "ERROR: UNIMPLEMENTED AWARD TEXT";
+    }
+  }
 }
